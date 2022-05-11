d8462e26c61ec0ea4abf037d1a0586b0cadaa3b8
Bruce0203 <BruceFreedy@gmail.com>
2022-05-11 10:26:26 +0900

    corrected required EventHanlder annotation

--- inggame-modules/minigame/api/src/main/kotlin/io/github/inggameteam/minigame/base/SpawnTeamPlayer.kt
+++ inggame-modules/minigame/api/src/main/kotlin/io/github/inggameteam/minigame/base/SpawnTeamPlayer.kt
@@ -3,9 +3,11 @@ package io.github.inggameteam.minigame.base
 import io.github.inggameteam.minigame.GameState
 import io.github.inggameteam.minigame.PTag
 import io.github.inggameteam.minigame.event.GPlayerSpawnEvent
+import org.bukkit.event.EventHandler
 
 interface SpawnTeamPlayer : SpawnPlayer {
 
+    @EventHandler
     override fun spawnPlayer(event: GPlayerSpawnEvent) {
         val player = event.player
         if (!isJoined(player)) return
