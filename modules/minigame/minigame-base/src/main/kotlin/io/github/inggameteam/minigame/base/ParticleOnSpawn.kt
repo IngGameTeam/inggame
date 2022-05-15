68cc6abbb48fea295db40eb3ec6059f5a1d12e36
Bruce0203 <BruceFreedy@gmail.com>
2022-05-15 16:03:13 +0900

    ParticleOnJoin -> ParticleOnSpawn

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/ParticleOnSpawn.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/ParticleOnSpawn.kt
@@ -1,15 +1,16 @@
 package io.github.inggameteam.minigame.base
 
 import io.github.inggameteam.minigame.Game
+import io.github.inggameteam.minigame.event.GPlayerSpawnEvent
 import io.github.inggameteam.minigame.event.GameJoinEvent
 import org.bukkit.Particle
 import org.bukkit.event.EventHandler
 
-interface ParticleOnJoin : Game {
+interface ParticleOnSpawn : Game {
 
     @Suppress("unused")
     @EventHandler
-    fun onJoinParticle(event: GameJoinEvent) {
+    fun onJoinParticle(event: GPlayerSpawnEvent) {
         val player = event.player
         if (!isJoined(player)) return
         player.spawnParticle(Particle.TOTEM, player.location, 10)
