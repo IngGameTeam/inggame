d9f5de16e0898c538365962623f69bd64bf16aa8
Bruce0203 <BruceFreedy@gmail.com>
2022-05-18 21:02:20 +0900

    corrected infinite respawn, minigame not stop bug

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/GoalIn.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/GoalIn.kt
@@ -3,22 +3,28 @@ package io.github.inggameteam.minigame.base
 import io.github.inggameteam.minigame.Game
 import io.github.inggameteam.minigame.PTag
 import io.github.inggameteam.player.hasTags
-import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerMoveEvent
 
-interface GoalIn : Game, Sectional {
+interface GoalIn : Game, Sectional, Competition {
 
     @Suppress("unused")
     @EventHandler
     fun moveGoalIn(event: PlayerMoveEvent) {
         val player = plugin[event.player]
         if (isJoined(player)) {
-            if (player.location.apply {
-                    if (block.type == Material.AIR) add(0.0, -1.0, 0.0)
-                }.toVector().isInAABB(getLocation("goal_pos1").toVector(), getLocation("goal_pos2").toVector())) {
-                joined.hasTags(PTag.PLAY).filter { player != it }
-                    .forEach { it.damage(10000.0) }
+            if (
+                player.location.toVector().isInAABB(
+                    getLocation("goal_pos1").toVector(),
+                    getLocation("goal_pos2").toVector()
+                )
+            )
+            {
+                joined
+                    .hasTags(PTag.PLAY)
+                    .filter { it != player }
+                    .forEach { it.addTag(PTag.DEAD); it.removeTag(PTag.PLAY) }
+                stopCheck()
             }
         }
     }
--- modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/angangang/game/impl/BoatRider.kt
+++ modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/angangang/game/impl/BoatRider.kt
@@ -1,13 +1,10 @@
 package io.github.inggameteam.minigame.angangang.game.impl
 
 import io.github.inggameteam.minigame.GamePlugin
-import io.github.inggameteam.minigame.base.NoDamage
-import io.github.inggameteam.minigame.base.NoInteract
-import io.github.inggameteam.minigame.base.Racing
-import io.github.inggameteam.minigame.base.SimpleGame
+import io.github.inggameteam.minigame.base.*
 import org.bukkit.entity.Boat
 
-class BoatRider(plugin: GamePlugin) : Racing(plugin), NoDamage, NoInteract, SimpleGame {
+class BoatRider(plugin: GamePlugin) : Racing(plugin), NoDamage, NoInteract, SimpleGame, GoalIn {
     override val name get() = "boat-rider"
     override fun getRider() = Boat::class.java
 }
