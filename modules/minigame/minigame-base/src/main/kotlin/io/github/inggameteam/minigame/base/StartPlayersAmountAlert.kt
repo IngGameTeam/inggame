d612659208946f7005c2e126e9fce788813d7688
Bruce0203 <BruceFreedy@gmail.com>
2022-05-20 01:17:18 +0900

    corrected to alert startPlayersAmount everyone joined

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/StartPlayersAmountAlert.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/StartPlayersAmountAlert.kt
@@ -8,18 +8,18 @@ import io.github.inggameteam.minigame.event.GameJoinEvent
 import io.github.inggameteam.player.hasTags
 import io.github.inggameteam.scheduler.delay
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 
 interface StartPlayersAmountAlert : Game {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     fun startPlayersAmountAlert(event: GameJoinEvent) {
         val player = event.player
         if (!isJoined(player)) return
         {
-            if (gameState == GameState.WAIT
-                && gameTask === null
-                && joined.hasTags(PTag.PLAY).size == 1
-                && startPlayersAmount > 0
+            if (gameState === GameState.WAIT
+                && joined.hasTags(PTag.PLAY).size < startPlayersAmount
+                && startPlayersAmount > 1
             ) joined.forEach { comp.send(GameAlert.NEED_PLAYER, it, displayName(it), startPlayersAmount) }
         }.delay(plugin, 0)
     }
