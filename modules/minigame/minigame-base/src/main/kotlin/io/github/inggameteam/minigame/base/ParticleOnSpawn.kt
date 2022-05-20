a9c180a7ef6bc440e7e91436118871829501a4aa
Bruce0203 <Bruce0203@users.noreply.github.com>
2022-05-20 03:19:01 +0000

    deploy: 2e865b6a9fee0705fd2127a80318c4bb918797a7

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/ParticleOnSpawn.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/ParticleOnSpawn.kt
@@ -5,15 +5,16 @@ import io.github.inggameteam.minigame.event.GPlayerSpawnEvent
 import io.github.inggameteam.minigame.event.GameJoinEvent
 import org.bukkit.Particle
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 
 interface ParticleOnSpawn : Game {
 
     @Suppress("unused")
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     fun onJoinParticle(event: GPlayerSpawnEvent) {
         val player = event.player
         if (!isJoined(player)) return
-        player.spawnParticle(Particle.TOTEM, player.location, 10)
+        player.location.world?.spawnParticle(Particle.TOTEM, player.location, 10)
     }
 
 }
