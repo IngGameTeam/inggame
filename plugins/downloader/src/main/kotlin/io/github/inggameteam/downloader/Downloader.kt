5269217c3f9e220ee5644217479e8d26d2cfa8d3
Bruce0203 <BruceFreedy@gmail.com>
2022-05-31 02:18:51 +0900

    add downloader plugin

--- plugins/downloader/src/main/kotlin/io/github/inggameteam/downloader/Downloader.kt
+++ plugins/downloader/src/main/kotlin/io/github/inggameteam/downloader/Downloader.kt
@@ -1,13 +1,31 @@
 package io.github.inggameteam.downloader
 
 import org.apache.commons.io.FileUtils
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 import java.net.URL
 
-object Downloader {
+@Suppress("unused")
+class Downloader : JavaPlugin(), CommandExecutor {
 
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (args.size >= 1) {
+            sender.sendMessage("Downloading...")
+            download(Bukkit.getPluginManager().getPlugin(args[0])!!)
+            sender.sendMessage("Downloaded")
+        }
+        return true
+    }
+
+    override fun onEnable() {
+        getCommand("downloader")?.setExecutor(this)
+    }
 
     fun download(plugin: Plugin) {
         val configFile = File(plugin.dataFolder, "downloader.yml")
