f45b9584cefb46f39183052e928c13cf4d942597
Bruce0203 <BruceFreedy@gmail.com>
2022-08-03 13:41:44 +0900

    add ItemType.SELECT

--- modules/item/item-impl/src/main/kotlin/io/github/inggameteam/item/impl/ItemType.kt
+++ modules/item/item-impl/src/main/kotlin/io/github/inggameteam/item/impl/ItemType.kt
@@ -3,6 +3,6 @@ package io.github.inggameteam.item.impl
 import io.github.inggameteam.item.api.ItemComponentGetter
 
 enum class ItemType {
-    VOLUME, TOGGLE, LIMITED;
+    VOLUME, TOGGLE, LIMITED, SELECT;
 }
 fun ItemComponentGetter.toItemType(name: String) = ItemType.valueOf(itemComp.string(name, plugin.defaultLanguage))
