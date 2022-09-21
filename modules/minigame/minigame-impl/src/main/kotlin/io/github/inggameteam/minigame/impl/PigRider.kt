7f38c471874e610079e9da8372656a8b763c820c
Bruce0203 <BruceFreedy@gmail.com>
2022-09-22 00:26:24 +0900

    test: PigRider startPlayerAmount to 1

--- modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/impl/PigRider.kt
+++ modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/impl/PigRider.kt
@@ -8,6 +8,7 @@ import org.bukkit.entity.Pig
 
 class PigRider(plugin: GamePlugin) : Racing(plugin), SimpleGame, NoDamage, NoInteract, GoalIn {
     override val name get() = "pig-rider"
+    override val startPlayersAmount get() = 1
     override fun getRider() = Pig::class.java
 
     override fun beginGame() {
