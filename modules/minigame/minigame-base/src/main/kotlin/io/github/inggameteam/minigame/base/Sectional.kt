e309bde751fb6cd6211274dade96b83f5e5b7638
Bruce0203 <BruceFreedy@gmail.com>
2022-10-10 17:15:01 +0900

    fix: stopWaitingTick to 10min from 5sec

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/Sectional.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/Sectional.kt
@@ -43,7 +43,7 @@ abstract class SectionalImpl(plugin: GamePlugin) : GameImpl(plugin), Sectional {
     /**
      * 할당된 구역 마무리 정리 시간
      */
-    override val stopWaitingTick = 20 * 5L
+    override val stopWaitingTick = 20 * 10L * 60
     override val schematicName by lazy { comp.stringListOrNull("schems", plugin.defaultLanguage)?.random()?: "default" }
 
     private val height get() = plugin.gameRegister.sectorHeight
