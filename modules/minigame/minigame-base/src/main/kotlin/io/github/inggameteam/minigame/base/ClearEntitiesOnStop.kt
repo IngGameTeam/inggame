82478ff33f3c144a13f225eb35b0224fc9fd9da1
Bruce0203 <BruceFreedy@gmail.com>
2022-10-12 22:52:06 +0900

    fix: issue #136

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/ClearEntitiesOnStop.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/ClearEntitiesOnStop.kt
@@ -1,6 +1,7 @@
 package io.github.inggameteam.minigame.base
 
 import io.github.inggameteam.minigame.event.GameUnloadEvent
+import org.bukkit.Location
 import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 
@@ -9,7 +10,11 @@ interface ClearEntitiesOnStop : Sectional {
     @Suppress("unused")
     @EventHandler
     fun onGameStop(event: GameUnloadEvent) {
-        point.world.getNearbyEntities(getLocation("start"), 100.0, 100.0, 100.0).forEach {
+        point.world.getNearbyEntities(Location(point.world,
+            point.x + plugin.gameRegister.sectorWidth.toDouble(),
+            plugin.gameRegister.sectorHeight.toDouble(),
+            point.y + plugin.gameRegister.sectorWidth.toDouble()
+        ), 150.0, 150.0, 150.0).forEach {
             if (it.type != EntityType.PLAYER) it.remove()
         }
     }
