3022faaa4036a3c80940835de0e54345f476c849
Bruce0203 <BruceFreedy@gmail.com>
2022-10-29 08:21:45 +0900

    fix: reduce raduis of loading chunk

--- modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/Chunky.kt
+++ modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/Chunky.kt
@@ -11,7 +11,7 @@ class Chunky(loc: Location) {
     init {
         try {
             val chunky: ChunkyAPI? = Bukkit.getServer().servicesManager.load(ChunkyAPI::class.java)
-        chunky!!.startTask(loc.world!!.name, "square", loc.x, loc.z, 10000.0, 10000.0,"concentric")
+        chunky!!.startTask(loc.world!!.name, "square", loc.x, loc.z, 1000.0, 1000.0,"concentric")
         chunky.onGenerationComplete { event -> Bukkit.getLogger().info("Generation completed for " + event.world()) }
         } catch (_: Exception) {
             println("Failed to starting Chunky")
