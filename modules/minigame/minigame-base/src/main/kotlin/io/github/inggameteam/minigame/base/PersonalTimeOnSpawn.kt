c98e5bc786ce2958b329299489cfac1e4066ba35
Bruce0203 <BruceFreedy@gmail.com>
2022-10-28 22:46:35 +0900

    feat: fix bug

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/PersonalTimeOnSpawn.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/PersonalTimeOnSpawn.kt
@@ -2,19 +2,34 @@ package io.github.inggameteam.minigame.base
 
 import io.github.inggameteam.minigame.Game
 import io.github.inggameteam.minigame.event.GPlayerSpawnEvent
+import io.github.inggameteam.minigame.event.GameJoinEvent
+import io.github.inggameteam.player.GPlayer
 import org.bukkit.event.EventHandler
 
 interface PersonalTimeOnSpawn : Game, Sectional {
 
     @Suppress("unused")
     @EventHandler
+    fun onGameJoin(event: GameJoinEvent) {
+        val player = event.player
+        if (isJoined(player)) {
+            setPlayerTime(player)
+        }
+    }
+
+    @Suppress("unused")
+    @EventHandler
     fun onSpawnSetPersonalTime(event: GPlayerSpawnEvent) {
         val player = event.player
         if (isInSector(player.location)) {
-            player.setPlayerTime(comp.intOrNull("time")?.toLong()?: return, false)
+            setPlayerTime(player)
         } else {
             player.resetPlayerTime()
         }
+    }
+
+    private fun setPlayerTime(player: GPlayer) {
+        player.setPlayerTime(comp.intOrNull("time")?.toLong()?: return, false)
     }
 
 }
