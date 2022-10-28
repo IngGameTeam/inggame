0de999ccdd7c5007bce1f101e5873b60bfefd903
Bruce0203 <BruceFreedy@gmail.com>
2022-10-29 07:49:29 +0900

    test: unloadChunk after paste schem

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/Sectional.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/Sectional.kt
@@ -120,9 +120,9 @@ abstract class SectionalImpl(plugin: GamePlugin) : GameImpl(plugin), Sectional {
         val z = sector.y * width
         val file = getSchematicFile(DEFAULT, DEFAULT_DIR)
         val location = Location(world, x.toDouble(), height.toDouble(), z.toDouble())
-        FaweImpl().unloadChunk(location, getSchematicFile(schematicName, this.name))
         ;{
             FaweImpl().paste(location, file)
+            FaweImpl().unloadChunk(location, getSchematicFile(schematicName, this.name))
             plugin.logger.info("$name unloaded $sector (${System.currentTimeMillis() - before}ms)")
         }.async(plugin)
     }
