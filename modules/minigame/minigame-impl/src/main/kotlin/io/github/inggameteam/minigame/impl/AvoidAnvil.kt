3761dd249feb033c5c87f62a6270aef4fa6cff96
Bruce0203 <BruceFreedy@gmail.com>
2022-11-02 22:32:20 +0900

    feat: now avoid anvil spawn period is faster than before

--- modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/impl/AvoidAnvil.kt
+++ modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/impl/AvoidAnvil.kt
@@ -46,7 +46,7 @@ class AvoidAnvil(plugin: GamePlugin) : SimpleGame, CompetitionImpl(plugin), Reco
         var count = 0.0
         addTask({
             (0..count.toInt()).forEach { _ -> spawnAnvilRandomly() }
-            count += 0.02
+            count += 0.08
             true
         }.repeat(plugin, 1, 1))
     }
