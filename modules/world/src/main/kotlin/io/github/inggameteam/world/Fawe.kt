7a21cfbbf3b665f5eb7a087e9ac27d6fafad570b
Bruce0203 <BruceFreedy@gmail.com>
2022-11-06 21:52:55 +0900

    fix: remove chunk load

--- modules/world/src/main/kotlin/io/github/inggameteam/world/Fawe.kt
+++ modules/world/src/main/kotlin/io/github/inggameteam/world/Fawe.kt
@@ -3,12 +3,10 @@ package io.github.inggameteam.world
 import com.fastasyncworldedit.core.FaweAPI
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldedit.math.BlockVector3
-import io.papermc.lib.PaperLib
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.plugin.Plugin
 import java.io.File
-import kotlin.system.measureTimeMillis
 
 
 val FAWE get() = Bukkit.getPluginManager().getPlugin("FastAsyncWorldEdit")
@@ -23,21 +21,21 @@ interface Fawe {
 open class FaweImpl(val plugin: Plugin) : Fawe {
 
     override fun loadChunk(location: Location, file: File) {
-        try {
-            if (file.exists().not()) return
-            FaweAPI.load(file).apply {
-                measureTimeMillis {
-                    for (addX in minimumPoint.x..maximumPoint.x)
-                        for (addY in minimumPoint.y..maximumPoint.y)
-                        {
-                            val world = location.world!!
-                            PaperLib.getChunkAtAsync(world, location.blockX + addX, location.blockZ + addY, true).get()
-                        }
-                }.apply { println("measureChunkLoadTimeMillis: $this") }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+//        try {
+//            if (file.exists().not()) return
+//            FaweAPI.load(file).apply {
+//                measureTimeMillis {
+//                    for (addX in minimumPoint.x..maximumPoint.x)
+//                        for (addY in minimumPoint.y..maximumPoint.y)
+//                        {
+//                            val world = location.world!!
+//                            PaperLib.getChunkAtAsync(world, location.blockX + addX, location.blockZ + addY, true).get()
+//                        }
+//                }.apply { println("measureChunkLoadTimeMillis: $this") }
+//            }
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
     }
 
     override fun unloadChunk(location: Location, file: File) {
