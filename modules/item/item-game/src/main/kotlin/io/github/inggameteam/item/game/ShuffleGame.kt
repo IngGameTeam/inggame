871bafad4b59804c059c87ff26c39c57322be986
Bruce0203 <BruceFreedy@gmail.com>
2022-11-06 23:32:03 +0900

    feat: add ShuffleGame

--- modules/item/item-game/src/main/kotlin/io/github/inggameteam/item/game/ShuffleGame.kt
+++ modules/item/item-game/src/main/kotlin/io/github/inggameteam/item/game/ShuffleGame.kt
@@ -11,20 +11,25 @@ import org.bukkit.event.EventHandler
 class ShuffleGame(override val plugin: GamePlugin) : Item, Interact {
     override val name: String = "shuffle-game"
     override fun use(name: String, player: GPlayer) {
+        player[SHUFFLE_KEY] = true
         shuffleJoin(player)
     }
 
     @Suppress("unused")
     @EventHandler
     fun onPlayerJoinHub(event: GameJoinEvent) {
-        if (plugin.gameRegister.hubName != event.join.name) return
-        if (event.player[SHUFFLE_KEY] !== null) {
-
+        if (plugin.gameRegister.hubName != event.join.name) {
+            event.player.remove(SHUFFLE_KEY)
+        } else {
+            if (event.player[SHUFFLE_KEY] !== null) {
+                shuffleJoin(event.player)
+            }
         }
     }
 
     fun shuffleJoin(player: GPlayer) {
         plugin.gameRegister.join(player, randomGame(player), forceCreateGame = true)
+        player[SHUFFLE_KEY] = true
     }
 
     fun randomGame(player: GPlayer): String {
