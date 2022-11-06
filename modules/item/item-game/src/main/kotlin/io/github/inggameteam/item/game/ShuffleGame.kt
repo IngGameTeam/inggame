6231731e5512b2b088c461cecb87d9e19f50b77e
Bruce0203 <BruceFreedy@gmail.com>
2022-11-07 00:25:45 +0900

    feat: add ShuffleGame

--- modules/item/item-game/src/main/kotlin/io/github/inggameteam/item/game/ShuffleGame.kt
+++ modules/item/item-game/src/main/kotlin/io/github/inggameteam/item/game/ShuffleGame.kt
@@ -4,10 +4,13 @@ import io.github.inggameteam.alert.Lang.lang
 import io.github.inggameteam.api.HandleListener
 import io.github.inggameteam.item.api.Interact
 import io.github.inggameteam.item.api.Item
+import io.github.inggameteam.minigame.Game
 import io.github.inggameteam.minigame.GamePlugin
+import io.github.inggameteam.minigame.GameState
 import io.github.inggameteam.minigame.event.GameJoinEvent
 import io.github.inggameteam.player.GPlayer
 import io.github.inggameteam.scheduler.delay
+import io.github.inggameteam.scheduler.runNow
 import org.bukkit.event.EventHandler
 
 class ShuffleGame(override val plugin: GamePlugin) : Item, Interact, HandleListener(plugin) {
@@ -17,21 +20,24 @@ class ShuffleGame(override val plugin: GamePlugin) : Item, Interact, HandleListe
         shuffleJoin(player)
     }
 
+
+
     @Suppress("unused")
     @EventHandler
     fun onPlayerJoinHub(event: GameJoinEvent) {
         if (plugin.gameRegister.hubName != event.join.name) {
             event.player.remove(SHUFFLE_KEY)
         } else {
-            if (event.player[SHUFFLE_KEY] !== null) {
-                { shuffleJoin(event.player) }.delay(plugin, 1)
+            val game = event.player[SHUFFLE_KEY] as? Game
+            if (game !== null && game.gameState === GameState.STOP) {
+                { shuffleJoin(event.player) }.runNow(plugin)
             }
         }
     }
 
     fun shuffleJoin(player: GPlayer) {
         plugin.gameRegister.join(player, randomGame(player), forceCreateGame = true)
-        player[SHUFFLE_KEY] = true
+        player[SHUFFLE_KEY] = plugin.gameRegister.getJoinedGame(player)
     }
 
     fun randomGame(player: GPlayer): String {
