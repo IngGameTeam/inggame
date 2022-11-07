a028e13dc8abc2ba82d2fd2ffa5e5f5967bf4ba2
Bruce0203 <BruceFreedy@gmail.com>
2022-11-07 16:49:35 +0900

    fix: ShuffleGame

--- modules/item/item-game/src/main/kotlin/io/github/inggameteam/item/game/ShuffleGame.kt
+++ modules/item/item-game/src/main/kotlin/io/github/inggameteam/item/game/ShuffleGame.kt
@@ -8,15 +8,14 @@ import io.github.inggameteam.minigame.Game
 import io.github.inggameteam.minigame.GamePlugin
 import io.github.inggameteam.minigame.GameState
 import io.github.inggameteam.minigame.event.GameJoinEvent
+import io.github.inggameteam.minigame.event.GameLeftEvent
 import io.github.inggameteam.player.GPlayer
-import io.github.inggameteam.scheduler.delay
 import io.github.inggameteam.scheduler.runNow
 import org.bukkit.event.EventHandler
 
 class ShuffleGame(override val plugin: GamePlugin) : Item, Interact, HandleListener(plugin) {
     override val name: String = "shuffle-game"
     override fun use(name: String, player: GPlayer) {
-        player[SHUFFLE_KEY] = true
         shuffleJoin(player)
     }
 
@@ -24,24 +23,22 @@ class ShuffleGame(override val plugin: GamePlugin) : Item, Interact, HandleListe
 
     @Suppress("unused")
     @EventHandler
-    fun onPlayerJoinHub(event: GameJoinEvent) {
-        if (plugin.gameRegister.hubName != event.join.name) {
-            event.player.remove(SHUFFLE_KEY)
-        } else {
+    fun onPlayerJoinHub(event: GameLeftEvent) {
+        if (plugin.gameRegister.hubName != event.left.name) {
             val game = event.player[SHUFFLE_KEY] as? Game
             if (game !== null && game.gameState === GameState.STOP) {
                 event.player.remove(SHUFFLE_KEY)
-                ; { shuffleJoin(event.player) }.runNow(plugin)
+                ;{ shuffleJoin(event.player) }.runNow(plugin)
             }
         }
     }
 
-    fun shuffleJoin(player: GPlayer) {
+    private fun shuffleJoin(player: GPlayer) {
         plugin.gameRegister.join(player, randomGame(player), forceCreateGame = true)
         player[SHUFFLE_KEY] = plugin.gameRegister.getJoinedGame(player)
     }
 
-    fun randomGame(player: GPlayer): String {
+    private fun randomGame(player: GPlayer): String {
         val games = itemComp.stringList("$name-games", player.lang(plugin))
         return games.random()
     }
