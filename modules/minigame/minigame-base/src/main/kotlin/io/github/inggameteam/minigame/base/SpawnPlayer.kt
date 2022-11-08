a00e70992f5a1846d2e6a92d78dfc6582a02bc8e
Bruce0203 <BruceFreedy@gmail.com>
2022-11-08 20:07:18 +0900

    fix: issue #173

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/SpawnPlayer.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/SpawnPlayer.kt
@@ -45,6 +45,7 @@ interface SpawnPlayer : Game, Sectional {
     fun inventorySpawn(player: GPlayer, spawn: String = this.gameState.toString()): Inventory? {
         if (!player.hasTag(PTag.PLAY)) {
             player.inventory.clear()
+            player.inventory.remove(player.itemOnCursor)
             return null
         }
         return comp.inventoryOrNull(spawn, player.lang(plugin))?.apply { player.inventory.contents = contents }
