f28f080da0be7eb33b564261e872852195cb5863
Bruce0203 <BruceFreedy@gmail.com>
2022-11-13 22:15:02 +0900

    chore: add dependency

--- modules/swear/swear-handle/build.gradle.kts
+++ modules/swear/swear-handle/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     api(project(":modules:swear:swear-api"))
     api(project(":modules:module-api"))
     api(project(":modules:scheduler"))
+    api(project(":modules:player"))
+    api(project(":modules:mongodb"))
 }
