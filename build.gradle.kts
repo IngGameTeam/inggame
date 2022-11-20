1894dc64fdc5de2dfa4e0313b88aff1ccf8e7cf8
Bruce0203 <BruceFreedy@gmail.com>
2022-11-20 21:48:06 +0900

    chore: remove oss.sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,13 +32,11 @@ allprojects {
 
     repositories {
         mavenCentral()
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
         maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/releases/") }
         maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
         maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/public/") }
         maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://libraries.minecraft.net/") }
-        maven { url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/") }
         maven { url = uri("https://repo.codemc.io/repository/maven-public/") }
         maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
     }
