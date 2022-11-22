2da3dcd5cb6ab50b7649b4464dd33c29437cba1c
Bruce0203 <BruceFreedy@gmail.com>
2022-11-22 18:18:07 +0900

    feat: cancel task before start

--- modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/Chunky.kt
+++ modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/Chunky.kt
@@ -11,8 +11,10 @@ class Chunky(loc: Location) {
     init {
         try {
             val size  = 3200
-            val chunky: ChunkyAPI? = Bukkit.getServer().servicesManager.load(ChunkyAPI::class.java)
-        chunky!!.startTask(loc.world!!.name, "square", loc.x + size/2, loc.z + size/2, size.toDouble(), size.toDouble(),"concentric")
+            val chunky = Bukkit.getServer().servicesManager.load(ChunkyAPI::class.java)!!
+            val name = loc.world!!.name
+            chunky.cancelTask(name)
+            chunky.startTask(name, "square", loc.x + size/2, loc.z + size/2, size.toDouble(), size.toDouble(),"concentric")
         } catch (_: Exception) {
             println("Failed to starting Chunky")
         }
