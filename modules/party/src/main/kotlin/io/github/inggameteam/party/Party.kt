8eb6e45ffe08a53a0667ccbecbea53eaa671006e
Bruce0203 <BruceFreedy@gmail.com>
2022-11-27 10:26:26 +0900

    feat: now Party visible(opened) default value is false

--- modules/party/src/main/kotlin/io/github/inggameteam/party/Party.kt
+++ modules/party/src/main/kotlin/io/github/inggameteam/party/Party.kt
@@ -17,7 +17,7 @@ const val PARTY = "party"
 class Party(
     override val plugin: PartyPlugin,
     val joined: GPlayerList = GPlayerList(),
-    var opened: Boolean = true,
+    var opened: Boolean = false,
     var renamed: Boolean = false,
     val banList: ArrayList<UUID> = ArrayList(),
     ) : PluginHolder<PartyPlugin> {
