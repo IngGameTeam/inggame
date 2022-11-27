dc9eec692f5a45c23faae94f747178f4393f6589
Bruce0203 <BruceFreedy@gmail.com>
2022-11-27 11:34:24 +0900

    fix: ChunkForceLoader

--- modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/ChunkForceLoader.kt
+++ modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/ChunkForceLoader.kt
@@ -12,18 +12,15 @@ class ChunkForceLoader(val plugin: GamePlugin) {
         var x = 0
         var y = 0
         ;{
+            plugin.gameRegister.worldName.forEach { worldName ->
+                loadChunkSector(Sector(x, y, Bukkit.getWorld(worldName)))
+            }
             if (x <= length) {
-                plugin.gameRegister.worldName.forEach { worldName ->
-                    loadChunkSector(Sector(x, y, Bukkit.getWorld(worldName)))
-                }
                 x++
                 true
-            } else if (y >= length) {
+            } else if (y > length) {
                 false
             } else {
-                plugin.gameRegister.worldName.forEach { worldName ->
-                    loadChunkSector(Sector(x, y, Bukkit.getWorld(worldName)))
-                }
                 x = 0
                 y++
                 true
@@ -38,12 +35,24 @@ class ChunkForceLoader(val plugin: GamePlugin) {
         val center = Sector(sector.x * width, sector.y * width)
         val min = Sector(center.x - halfWidth, center.y - halfWidth)
         val max = Sector(center.x + halfWidth, center.y + halfWidth)
-        for (x in min.x until max.x) for (y in min.y until max.y) {
+        var x = min.x
+        var y = min.y
+        ;{
             world.getChunkAt(x, y).apply {
                 load(false)
                 isForceLoaded = true
             }
-        }
+            if (x <= max.x) {
+                x += 16
+                true
+            } else if(y > max.y) {
+                false
+            } else {
+                x = 0
+                y += 16
+                true
+            }
+        }.repeat(plugin, 1L, 1L)
     }
 
 }
