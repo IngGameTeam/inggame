9f28ad3540d41f53c557dfc6770add5d4d6ffab5
Bruce0203 <BruceFreedy@gmail.com>
2022-12-10 20:56:08 +0900

    test: context.path()

--- modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/RestServer.kt
+++ modules/minigame/minigame-handle/src/main/kotlin/io/github/inggameteam/minigame/handle/RestServer.kt
@@ -2,30 +2,33 @@ package io.github.inggameteam.minigame.handle
 
 import io.github.inggameteam.minigame.GamePlugin
 import io.javalin.Javalin
+import io.javalin.http.Context
 import io.javalin.http.Header
 import org.bukkit.Bukkit
 import kotlin.concurrent.thread
 
 class RestServer(val plugin: GamePlugin) {
 
+    val app by lazy { Javalin.create(/*config*/) }
+
     init {
         thread {
-            val app = Javalin.create(/*config*/)
-                .get("/game-stats") { ctx ->
-                    ctx.header(Header.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-                    ctx.json(
-                    """{
-                    "message": "Hello World",
-                    "online": "${Bukkit.getOnlinePlayers().size}"
-                    }""".trimIndent()
-                )
+            "/game-stats" get { """{"online": "${Bukkit.getOnlinePlayers().size}"}""" }
+            "chat" get { ctx ->
+                println(ctx.path())
+                ""
             }
-                .start(8080)
+            app.start(8080)
+            plugin.addDisableEvent { app.stop() }
             println("RestServer Started!")
-            plugin.addDisableEvent {
-                app.stop()
-            }
 
+        }
+    }
+
+    infix fun String.get(block: (Context) -> String) {
+        app.get(this) { ctx ->
+            ctx.header(Header.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            ctx.json(block(ctx))
         }
     }
 
