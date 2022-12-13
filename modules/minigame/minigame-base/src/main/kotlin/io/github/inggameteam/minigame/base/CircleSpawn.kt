d9602679a9f25f786876f17d62a7fcb4635fa2da
Bruce0203 <BruceFreedy@gmail.com>
2022-12-13 20:59:51 +0900

    fix: circleSpawn only Playing

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/CircleSpawn.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/CircleSpawn.kt
@@ -1,5 +1,6 @@
 package io.github.inggameteam.minigame.base
 
+import io.github.inggameteam.minigame.GameState
 import io.github.inggameteam.minigame.PTag
 import io.github.inggameteam.player.GPlayer
 import io.github.inggameteam.player.hasTags
@@ -10,6 +11,7 @@ import kotlin.math.sin
 interface CircleSpawn : Sectional, SpawnPlayer {
 
     override fun tpSpawn(player: GPlayer, spawn: String): Location? {
+        if (gameState !== GameState.PLAY) return null
         val center = super.tpSpawn(player, spawn) ?: throw AssertionError("no spawn location")
         val players = joined.hasTags(PTag.PLAY)
         val i = players.indexOf(player)
