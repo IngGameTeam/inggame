d1fbd9185f5131af3f659f1f461fc9b49dffb938
Bruce0203 <BruceFreedy@gmail.com>
2022-12-19 17:56:01 +0900

    fix: plugin

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ do
       git clone -b bukkit --single-branch https://github.com/IngGameTeam/inggame
   fi
   cd inggame
-  cd plugins
+  mkdir -r plugins
   #sudo apt install -y jq
   function plugin() {
       latest=$(curl -sL https://api.github.com/repos/$1/releases/latest | jq -r ".tag_name")
@@ -25,6 +25,7 @@ do
   plugin ViaVersion/ViaVersion ViaVersion
   plugin ViaVersion/ViaBackwards ViaBackwards
   wget -O server.jar https://ci.codemc.io/job/etil2jz/job/Mirai-1.19/lastSuccessfulBuild/artifact/build/libs/mirai-paperclip-1.19.2-R0.1-SNAPSHOT-reobf.jar
+
 mkdir customized_minigame
 cp -r customized_minigame-config.yml customized_minigame/paper-world.yml
 java -Xms12G -Xmx12G -jar server.jar
