5ac523bd328afb6de3997bbb6864dcbf46a2daba
Bruce0203 <BruceFreedy@gmail.com>
2022-12-19 17:40:03 +0900

    fix: run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 while :
 do
+chmod +x ./update.sh
 ./update.sh
 mkdir customized_minigame
 cp -r customized_minigame-config.yml customized_minigame/paper-world.yml
