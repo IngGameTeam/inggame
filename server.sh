4821b072fd9ac213f1e6b91b90ea2e450787492d
Bruce0203 <BruceFreedy@gmail.com>
2023-01-11 17:41:05 +0900

    set url remote on pull

--- server.sh
+++ server.sh
@@ -1,3 +1,5 @@
+GIT_URL=https://Bruce0203:$GH_TOKEN@github.com/IngGameTeam/inggame
+
 #BASEDIR=$(dirname $0)
 #cd $BASEDIR
   if [ -d ".git" ]; then
@@ -5,13 +7,14 @@
       cd plugins
       git stash
       git stash drop
+      git remote set-url origin $GIT_URL
       git pull origin bukkit
       cd ..
       cd ..
   else
       echo "clone!"
       cd ..
-      git clone -b bukkit --single-branch https://Bruce0203:$GH_TOKEN@github.com/IngGameTeam/inggame
+      git clone -b bukkit --single-branch $GIT_URL
   fi
   cd inggame
   #sudo apt install -y jq
