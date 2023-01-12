2c3151f3b41f5af16faba7843ad26b35a00b8dd7
Bruce0203 <BruceFreedy@gmail.com>
2023-01-13 01:18:54 +0900

    test: WIP

--- player/src/main/kotlin/io/github/inggameteam/inggame/player/Module.kt
+++ player/src/main/kotlin/io/github/inggameteam/inggame/player/Module.kt
@@ -4,7 +4,7 @@ import io.github.inggameteam.inggame.player.handler.PlayerLoader
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-fun createPlayerModule(collection: String) = module(createdAtStart = true) {
+fun createPlayerModule(collection: String) = module {
     single { PlayerService(get(named(collection)))}
     single { PlayerLoader(get(), get()) }
 }
