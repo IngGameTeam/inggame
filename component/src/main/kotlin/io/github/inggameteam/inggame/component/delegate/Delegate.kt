e8a5a9b0c3481c97b1b43b9db3d87b36a5418e89
Bruce0203 <BruceFreedy@gmail.com>
2023-01-12 22:07:35 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/delegate/Delegate.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/delegate/Delegate.kt
@@ -51,7 +51,6 @@ class NullableDelegateImp(
     override operator fun <T, R> getValue(thisRef: T, property: KProperty<*>): R? {
         val result = try {
             component[nameSpace, property.name, Any::class]
-            println("12334owirjgoawrh982h4t98214")
         } catch (_: Exception) {
             println("exceptionexceptionexceptionexceptionexception")
             defaultBlock?.invoke()?.apply { setValue(thisRef, property, this) }
