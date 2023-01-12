261fd605aec8480f46ea40aa5122cda5442480e5
Bruce0203 <BruceFreedy@gmail.com>
2023-01-13 01:33:47 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/GameResourceService.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/GameResourceService.kt
@@ -2,9 +2,10 @@ package io.github.inggameteam.inggame.minigame
 
 import io.github.inggameteam.inggame.component.componentservice.ComponentService
 import io.github.inggameteam.inggame.component.componentservice.LayeredComponentService
+import io.github.inggameteam.inggame.component.componentservice.ResourceComponentService
 
 class GameResourceService(componentService: ComponentService)
-    : LayeredComponentService by componentService as LayeredComponentService
+    : ResourceComponentService by componentService as ResourceComponentService
 {
 
     fun a() {
