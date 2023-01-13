0825c0ce7733c6b49025544467ca41a20e86b47f
Bruce0203 <BruceFreedy@gmail.com>
2023-01-13 14:35:42 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/PrintOnMove.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/PrintOnMove.kt
@@ -1,9 +1,5 @@
 package io.github.inggameteam.inggame.minigame.handler
 
-import io.github.inggameteam.inggame.component.delegate.get
-import io.github.inggameteam.inggame.minigame.wrapper.GameServer
-import io.github.inggameteam.inggame.minigame.wrapper.player.GPlayer
-import io.github.inggameteam.inggame.player.PlayerService
 import io.github.inggameteam.inggame.utils.HandleListener
 import io.github.inggameteam.inggame.utils.IngGamePlugin
 import org.bukkit.event.EventHandler
