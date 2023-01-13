4ad40b0832bff443c5d6c570be08654b751ed510
Bruce0203 <BruceFreedy@gmail.com>
2023-01-13 22:40:20 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/PrintOnMove.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/PrintOnMove.kt
@@ -21,7 +21,7 @@ class PrintOnMove(
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
         val player = event.player.uniqueId
-        println(playerService.get("Hub"))
+        println(playerService.get("hub"))
         if (playerService.has(player, "PrintOnMove")) {
             event.player.sendMessage("PrintOnMove")
         }
