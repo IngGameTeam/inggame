a9ee2e36d53187b3e100eec7465d24c086f9188c
Bruce0203 <BruceFreedy@gmail.com>
2023-01-13 22:35:17 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/PrintOnMove.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/PrintOnMove.kt
@@ -1,6 +1,7 @@
 package io.github.inggameteam.inggame.minigame.handler
 
 import io.github.inggameteam.inggame.component.delegate.get
+import io.github.inggameteam.inggame.minigame.GameResourceService
 import io.github.inggameteam.inggame.minigame.wrapper.GameServer
 import io.github.inggameteam.inggame.minigame.wrapper.player.GPlayer
 import io.github.inggameteam.inggame.player.PlayerService
@@ -12,6 +13,7 @@ import org.bukkit.event.player.PlayerMoveEvent
 class PrintOnMove(
     private val server: GameServer,
     private val playerService: PlayerService,
+    private val gameResourceService: GameResourceService,
     plugin: IngGamePlugin
 ) : HandleListener(plugin) {
 
@@ -19,7 +21,7 @@ class PrintOnMove(
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
         val player = event.player.uniqueId
-        println(playerService.get("PrintOnMove"))
+        println(gameResourceService.get("PrintOnMove"))
         if (playerService.has(player, "PrintOnMove")) {
             event.player.sendMessage("PrintOnMove")
         }
