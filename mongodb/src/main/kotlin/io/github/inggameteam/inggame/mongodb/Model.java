abef85306370741d39b17bb343910d86427f5bed
Bruce0203 <BruceFreedy@gmail.com>
2023-01-14 17:24:34 +0900

    test: WIP

---
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/Model.java
@@ -0,0 +1,7 @@
+package io.github.inggameteam.inggame.mongodb;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Model {}
--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/Model.kt
+++
@@ -1,4 +0,0 @@
-package io.github.inggameteam.inggame.mongodb
-
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Model
