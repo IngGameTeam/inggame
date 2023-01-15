101342cf51f4e12d4861143ddce4593e209cb93c
Bruce0203 <BruceFreedy@gmail.com>
2023-01-15 21:30:53 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoClient.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoClient.kt
@@ -5,18 +5,8 @@ import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import org.bson.UuidRepresentation
-import org.slf4j.LoggerFactory
-import java.io.File
 
 fun createClient(connectionString: ConnectionString, codec: MongoCodec): MongoClient {
-
-    val file = File("libraries/org/slf4j/slf4j-api/1.7.32/log4j2.xml")
-    file.createNewFile()
-    file.writeText("""
-<category name="org.mongodb.driver">
-    <priority value="OFF" />
-</category>
-    """.trimIndent())
     val clientSettings = MongoClientSettings.builder()
         .uuidRepresentation(UuidRepresentation.STANDARD)
         .applyConnectionString(connectionString)
