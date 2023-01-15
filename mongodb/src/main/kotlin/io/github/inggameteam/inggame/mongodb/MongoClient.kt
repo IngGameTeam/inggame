b4be8bacab70e0eba895db6317a60457db242d37
Bruce0203 <BruceFreedy@gmail.com>
2023-01-15 20:51:56 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoClient.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoClient.kt
@@ -1,21 +1,25 @@
 package io.github.inggameteam.inggame.mongodb
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import org.bson.UuidRepresentation
-import ch.qos.logback.classic.Logger
 import org.slf4j.LoggerFactory
 
 fun createClient(connectionString: ConnectionString, codec: MongoCodec): MongoClient {
 
     listOf(
+        "org.mongodb.driver",
         "org.mongodb.driver.cluster",
         "org.mongodb.driver.client",
     ).forEach {
-        val logger = LoggerFactory.getLogger(it) as Logger
-        logger.setLevel(ch.qos.logback.classic.Level.OFF);
+        val loggerContext: LoggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
+        val rootLogger: Logger = loggerContext.getLogger(it)
+        rootLogger.level = Level.OFF
     }
     val clientSettings = MongoClientSettings.builder()
         .uuidRepresentation(UuidRepresentation.STANDARD)
