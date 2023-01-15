dc276d4286861cada783070e98e5b01e2cbeaf38
Bruce0203 <BruceFreedy@gmail.com>
2023-01-15 14:49:07 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/delegate/Delegate.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/delegate/Delegate.kt
@@ -52,8 +52,7 @@ abstract class BaseDelegate : Delegate {
     }
 
     override fun equals(other: Any?): Boolean {
-        if (this.nameSpace == other) return true
-        return false
+        return this.nameSpace == other
     }
 
 
