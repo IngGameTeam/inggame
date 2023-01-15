036f84ba3cef59b5cd711339253ccd50253f7fcf
Bruce0203 <BruceFreedy@gmail.com>
2023-01-15 19:56:00 +0900

    test: WIP

--- plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/Plugin.kt
+++ plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/Plugin.kt
@@ -17,7 +17,6 @@ class Plugin : IngGamePluginImp {
     private var appSemaphore = false
     private val appDelegate = lazy {
         if (appSemaphore) throw AssertionError("an error occurred while get app while initializing app")
-        println("APP_LOAD")
         appSemaphore = true
         val result = loadApp(this)
         appSemaphore = false
