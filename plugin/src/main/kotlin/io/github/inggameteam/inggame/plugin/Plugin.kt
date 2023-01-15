4fe1c586abb0cd29ac5e1bb7150d7c6fcf492951
Bruce0203 <BruceFreedy@gmail.com>
2023-01-15 16:25:45 +0900

    test: WIP

--- plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/Plugin.kt
+++ plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/Plugin.kt
@@ -90,7 +90,7 @@ class Plugin : IngGamePluginImp() {
 
     override fun onEnable() {
         super.onEnable()
-        println("HELLO1!1")
+        println("HELLO1!1435")
         repeat(10) {
             println("-".repeat(30))
         }
