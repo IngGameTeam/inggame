f01ce00682489dfb751a72954a3c2d1fa88e7c7b
Bruce0203 <BruceFreedy@gmail.com>
2023-01-15 20:28:17 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoClient.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoClient.kt
@@ -10,7 +10,7 @@ import java.util.logging.Logger
 
 fun createClient(connectionString: ConnectionString, codec: MongoCodec): MongoClient {
     val mongoLogger = Logger.getLogger("org.mongodb.driver")
-    mongoLogger.level = Level.SEVERE
+    mongoLogger.level = Level.OFF
 
 
     val clientSettings = MongoClientSettings.builder()
