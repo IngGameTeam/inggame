ee8be0daec69dd50a1e018330d6d50afadac962c
Bruce0203 <BruceFreedy@gmail.com>
2023-01-17 02:49:18 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/NonElement.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/NonElement.kt
@@ -1,5 +1,5 @@
 package io.github.inggameteam.inggame.component
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class NonElement
