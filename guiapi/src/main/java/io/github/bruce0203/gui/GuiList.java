1a30d54be82c7d987ef57d53cd77c74cd597a582
Bruce0203 <BruceFreedy@gmail.com>
2023-01-16 15:41:58 +0900

    test: WIP

--- guiapi/src/main/java/io/github/bruce0203/gui/GuiList.java
+++ guiapi/src/main/java/io/github/bruce0203/gui/GuiList.java
@@ -48,7 +48,7 @@ public class GuiList<T> extends GuiRegion {
                 } else {
                     ind = ind % size;
                 }
-                if (ind < (x * y <= size ? size : size + this.index)) {
+                if (ind <= (x * y < size ? size : size + this.index)) {
                     T context = list.get(ind % size);
                     item = transform.apply(context);
                     display.add(new Pair<>(item, context));
