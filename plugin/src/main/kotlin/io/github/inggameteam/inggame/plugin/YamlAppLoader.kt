ad5434cc8732e818ab87350170dc6b6c95b826d6
Bruce0203 <BruceFreedy@gmail.com>
2023-01-17 21:11:51 +0900

    test: WIP

--- plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/YamlAppLoader.kt
+++ plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/YamlAppLoader.kt
@@ -30,13 +30,13 @@ fun loadApp(plugin: IngGamePlugin): Koin {
                     getKeys(false).map { key -> createRepo(key, getString(key)!!) }.toTypedArray()
                 } ?: emptyArray(),
                 *config.getConfigurationSection("file")?.run {
-                    getKeys(false).map { key -> createFileRepo(key, getString(key)!!.apply(::println)) }.toTypedArray()
+                    getKeys(false).map { key -> createFileRepo(key, getString(key)!!) }.toTypedArray()
                 } ?: emptyArray(),
                 *config.getConfigurationSection("layer")?.run {
-                    getKeys(false).map { key -> createLayer(key, getString(key)!!) }.toTypedArray()
+                    getKeys(false).map { key -> createLayer(key, getString(key)!!.apply(::println)) }.toTypedArray()
                 } ?: emptyArray(),
                 *config.getConfigurationSection("resource")?.run {
-                    getKeys(false).map { key -> createResource(key, getString(key)!!) }.toTypedArray()
+                    getKeys(false).map { key -> createResource(key, getString(key)!!.apply(::println)) }.toTypedArray()
                 } ?: emptyArray(),
                 *config.getConfigurationSection("save")?.run {
                     getKeys(false).map { key -> addToSaveRegistry(key) }.toTypedArray()
