a9e62126d9b7296bb5217aeb5b031eb9e65b168a
Bruce0203 <BruceFreedy@gmail.com>
2023-01-18 01:36:13 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/view/editor/model/NameSpaceParentsViewImp.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/view/editor/model/NameSpaceParentsViewImp.kt
@@ -2,11 +2,27 @@ package io.github.inggameteam.inggame.component.view.editor.model
 
 import io.github.inggameteam.inggame.component.NameSpace
 import io.github.inggameteam.inggame.component.componentservice.ComponentService
+import io.github.inggameteam.inggame.component.view.editor.selectors.Selector
 import io.github.inggameteam.inggame.utils.IngGamePlugin
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
 import org.koin.core.Koin
 
 data class NameSpaceParentsViewImp(
     override val componentService: ComponentService,
     override val app: Koin,
     override val nameSpace: NameSpace, override val plugin: IngGamePlugin
-) : NameSpaceParentsView
+) : NameSpaceParentsView {
+    override val elements: Collection<NameSpace>
+        get() = TODO("Not yet implemented")
+    override val parentSelector: Selector<*>?
+        get() = TODO("Not yet implemented")
+
+    override fun transform(t: NameSpace): ItemStack {
+        TODO("Not yet implemented")
+    }
+
+    override fun select(t: NameSpace, event: InventoryClickEvent) {
+        TODO("Not yet implemented")
+    }
+}
