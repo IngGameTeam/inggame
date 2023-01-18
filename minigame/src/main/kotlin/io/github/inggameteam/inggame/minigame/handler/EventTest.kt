da924414f6ab45b537bb2377d8190bb7df743d96
Bruce0203 <BruceFreedy@gmail.com>
2023-01-18 18:41:12 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/EventTest.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/EventTest.kt
@@ -2,15 +2,18 @@ package io.github.inggameteam.inggame.minigame.handler
 
 import io.github.inggameteam.inggame.utils.HandleListener
 import io.github.inggameteam.inggame.utils.IngGamePlugin
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
 import org.bukkit.plugin.Plugin
 
 class EventTest(plugin: IngGamePlugin) : HandleListener(plugin) {
 
     @EventHandler
-    fun onItemHeld(event: PlayerItemHeldEvent) {
-        event.player.sendMessage("PlayerItemHeldEvent")
+    fun onItemHeld(event: InventoryClickEvent ) {
+        val player = event.whoClicked as Player
+        player.sendMessage("PlayerItemHeldEvent{action=${event.action}}")
     }
 }
