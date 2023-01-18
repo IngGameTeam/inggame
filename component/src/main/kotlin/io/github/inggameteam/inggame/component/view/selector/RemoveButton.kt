5068a1d11a5b603b076530ac04350b4532b34afc
Bruce0203 <BruceFreedy@gmail.com>
2023-01-19 00:28:15 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/RemoveButton.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/RemoveButton.kt
@@ -12,7 +12,7 @@ interface RemoveButton<T : Any> : Selector<T> {
     override fun gui(gui: GuiFrameDSL) {
         super.gui(gui)
         gui.apply {
-            val removeItem = createItem(Material.LIME_DYE, view[selector, "remove-button", String::class])
+            val removeItem = createItem(Material.RED_DYE, view[selector, "remove-button", String::class])
             slot(6, 5, removeItem) { event ->
                 removeButton(event.whoClicked as Player)
             }
