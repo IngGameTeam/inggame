e8138f4510bb7c7299ef23f4f115849368fb5851
Bruce0203 <BruceFreedy@gmail.com>
2023-01-18 22:59:46 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/view/model/editor/ElementEditorView.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/view/model/editor/ElementEditorView.kt
@@ -3,7 +3,7 @@ package io.github.inggameteam.inggame.component.view.model.editor
 @Suppress("UNCHECKED_CAST")
 interface ElementEditorView<T : Any> : EditorView<T>, ElementView {
 
-    override val get: () -> T? get() = { componentService[nameSpace, element.first, Any::class] as T }
-    override val set: (T) -> Unit get() = { println("$nameSpace, ${element.first}, $it"); componentService.set(nameSpace, element.first, it) }
+    override val get: () -> T? get() = { componentService[nameSpace.name, element.first, Any::class] as T }
+    override val set: (T) -> Unit get() = { componentService.set(nameSpace.name, element.first, it) }
 
 }
