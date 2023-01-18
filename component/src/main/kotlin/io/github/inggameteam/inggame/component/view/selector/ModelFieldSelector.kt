0ffa8d3bcc4260d43d58a65f11607e0b3bfcfbd5
Bruce0203 <BruceFreedy@gmail.com>
2023-01-19 03:43:16 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/ModelFieldSelector.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/ModelFieldSelector.kt
@@ -5,6 +5,7 @@ import io.github.inggameteam.inggame.component.view.createItem
 import io.github.inggameteam.inggame.component.view.editor.Editor
 import io.github.inggameteam.inggame.component.view.model.ModelView
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.declaredMemberProperties
@@ -22,6 +23,7 @@ class ModelFieldSelector(
 
     override fun select(t: Field, event: InventoryClickEvent) {
         app.get<EditorRegistry>().getEditor(t.returnType, this, parentSelector)
+            .open(event.whoClicked as Player)
     }
 
     override fun transform(t: Field) = createItem(Material.OAK_PLANKS, t.name)
