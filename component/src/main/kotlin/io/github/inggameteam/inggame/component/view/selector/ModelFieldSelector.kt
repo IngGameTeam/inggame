2c4a10e6d5d38cde659fd883d5fcd0e816340a65
Bruce0203 <BruceFreedy@gmail.com>
2023-01-19 03:41:52 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/ModelFieldSelector.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/ModelFieldSelector.kt
@@ -1,20 +1,13 @@
 package io.github.inggameteam.inggame.component.view.selector
 
-import io.github.inggameteam.inggame.component.PropertyRegistry
 import io.github.inggameteam.inggame.component.view.EditorRegistry
 import io.github.inggameteam.inggame.component.view.createItem
 import io.github.inggameteam.inggame.component.view.editor.Editor
 import io.github.inggameteam.inggame.component.view.model.ModelView
-import org.bson.codecs.pojo.annotations.BsonIgnore
 import org.bukkit.Material
-import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
-import java.lang.reflect.Modifier
 import kotlin.reflect.KProperty
-import kotlin.reflect.KVisibility
 import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.jvm.kotlinProperty
 
 typealias Field = KProperty<*>
 class ModelFieldSelector(
