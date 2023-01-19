0e6bd08e54b1d937dd2850744fe9026278a27167
Bruce0203 <BruceFreedy@gmail.com>
2023-01-19 23:51:15 +0900

    fix: newAllocatable

--- modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/impl/Hub.kt
+++ modules/minigame/minigame-impl/src/main/kotlin/io/github/inggameteam/minigame/impl/Hub.kt
@@ -1,13 +1,18 @@
 package io.github.inggameteam.minigame.impl
 
 import io.github.inggameteam.minigame.GamePlugin
+import io.github.inggameteam.minigame.GameState
 import io.github.inggameteam.minigame.JoinType
 import io.github.inggameteam.minigame.base.*
 import io.github.inggameteam.minigame.base.Hub
 import io.github.inggameteam.minigame.event.GPlayerSpawnEvent
 import io.github.inggameteam.player.GPlayer
 import org.bukkit.Material
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+import org.bukkit.event.Cancellable
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.inventory.Inventory
 
 class Hub(plugin: GamePlugin) : Hub(plugin), SpawnPlayer, SpawnOnJoin, VoidDeath, SpawnHealth,
@@ -15,6 +20,12 @@ class Hub(plugin: GamePlugin) : Hub(plugin), SpawnPlayer, SpawnOnJoin, VoidDeath
     PersonalTimeOnSpawn, InteractingBan {
 
     override val noInteracts = listOf(Material.CHEST, Material.ENDER_CHEST)
+
+    override fun staticBreak(player: Player, material: Material, event: Cancellable) {
+        if ((isJoined(player) || isInSector(player.location)) && noInteracts.contains(material)) {
+            event.isCancelled = true
+        }
+    }
 
     @EventHandler
     override fun onJoinParticle(event: GPlayerSpawnEvent) {
