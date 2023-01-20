21bdf6d91cd13513d66a46607ec29aa984555a53
Bruce0203 <BruceFreedy@gmail.com>
2023-01-20 17:51:43 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
@@ -26,6 +26,7 @@ class MongoCodec(codecs: Collection<Class<*>>) {
 
     fun encode(value: Any): Any {
         return if (value.javaClass.getAnnotation(Model::class.java) !== null) {
+            println(value.javaClass)
             val document = BsonDocument()
             val writer = BsonDocumentWriter(document)
             codecRegistry[value.javaClass].encode(writer, value, EncoderContext.builder().build())
