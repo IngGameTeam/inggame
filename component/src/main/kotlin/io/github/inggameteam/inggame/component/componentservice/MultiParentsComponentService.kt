bc0ef3195d2654a2ffbc51c639f0984370b23afd
Bruce0203 <BruceFreedy@gmail.com>
2023-01-24 02:02:38 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/componentservice/MultiParentsComponentService.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/componentservice/MultiParentsComponentService.kt
@@ -23,25 +23,30 @@ class MultiParentsComponentService(
 
     override val layerPriority: Int by lazy { return@lazy components.maxOf { it.layerPriority } }
 
-    private fun findParent(nameSpace: Any): ComponentService {
+    private fun findParent(nameSpace: Any): Collection<ComponentService> {
         return try { rootComponent()!![nameSpace, parentKey!!, Any::class]
-            .let { name -> components.firstOrNull { it.name == name }!! } }
-        catch (_: Throwable) { components.first() }
+            .let { name -> components.firstOrNull { it.name == name }!! }.run(::listOf) }
+        catch (_: Throwable) { components }
+    }
+
+    fun <T, R> Iterable<T>.firstSuccess(block: (T) -> R, throws: Throwable): R {
+        this.forEach { try { return block(it) } catch (_: Throwable) { } }
+        throw throws
     }
 
     override fun <T : Any> get(nameSpace: Any, key: Any, clazz: KClass<T>): T {
         if (parentKey == key)
             throw AssertionError("an error occurred while perform get method parentKey and key is same")
         val nameSpace = uncoverDelegate(nameSpace)
-        return findParent(nameSpace)[nameSpace, key, clazz]
+        return findParent(nameSpace).firstSuccess({ it[nameSpace, key, clazz] }, NameSpaceNotFoundException(nameSpace))
     }
 
     override fun set(nameSpace: Any, key: Any, value: Any?) {
-        findParent(nameSpace).set(nameSpace, key, value)
+        findParent(nameSpace).first().set(nameSpace, key, value)
     }
 
     override fun getOrNull(name: Any): NameSpace? {
-        return findParent(name).getOrNull(name)
+        return findParent(name).firstSuccess({ getOrNull(name) }, NameSpaceNotFoundException(name))
     }
 
     override fun getAll(): Collection<NameSpace> {
