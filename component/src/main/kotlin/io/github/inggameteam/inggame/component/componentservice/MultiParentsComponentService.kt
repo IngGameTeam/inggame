739e2dd4c726a40729cdedac45d6cc742cbc31db
Bruce0203 <BruceFreedy@gmail.com>
2023-01-28 23:35:27 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/componentservice/MultiParentsComponentService.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/componentservice/MultiParentsComponentService.kt
@@ -35,7 +35,7 @@ class MultiParentsComponentService(
     }
 
     override fun <T : Any> get(nameSpace: Any, key: Any, clazz: KClass<T>): T {
-//        println("!!$name")
+        println("!!$name")
         if (parentKey == key)
             throw AssertionError("an error occurred while perform get method parentKey and key is same")
         val nameSpace = uncoverDelegate(nameSpace)
