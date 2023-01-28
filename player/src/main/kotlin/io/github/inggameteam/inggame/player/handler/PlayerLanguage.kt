a2868fff79414989b9689a20ff46fc5e8047b0fc
Bruce0203 <BruceFreedy@gmail.com>
2023-01-28 23:43:55 +0900

    test: WIP

--- player/src/main/kotlin/io/github/inggameteam/inggame/player/handler/PlayerLanguage.kt
+++ player/src/main/kotlin/io/github/inggameteam/inggame/player/handler/PlayerLanguage.kt
@@ -3,11 +3,12 @@ package io.github.inggameteam.inggame.player.handler
 import io.github.inggameteam.inggame.player.PlayerInstanceService
 import io.github.inggameteam.inggame.player.PlayerService
 import io.github.inggameteam.inggame.utils.HandleListener
+import io.github.inggameteam.inggame.utils.IngGamePlugin
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.Plugin
 
-class PlayerLanguage(val playerInstanceService: PlayerInstanceService, plugin: Plugin) : HandleListener(plugin) {
+class PlayerLanguage(val playerInstanceService: PlayerInstanceService, plugin: IngGamePlugin) : HandleListener(plugin) {
 
     @Suppress("unused")
     @EventHandler
