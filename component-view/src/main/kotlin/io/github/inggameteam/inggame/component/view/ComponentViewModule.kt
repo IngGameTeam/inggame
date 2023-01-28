32dcb1414d13cc019cff41fed3bb7f9fc4c661e5
Bruce0203 <BruceFreedy@gmail.com>
2023-01-29 02:47:13 +0900

    test: WIP

--- component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/ComponentViewModule.kt
+++ component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/ComponentViewModule.kt
@@ -1,11 +1,13 @@
 package io.github.inggameteam.inggame.component.view
 
 import io.github.inggameteam.inggame.component.event.ComponentServiceRegisterEvent
+import io.github.inggameteam.inggame.component.event.newModule
 import io.github.inggameteam.inggame.component.view.handler.ViewPlayerLoader
 import io.github.inggameteam.inggame.utils.HandleListener
 import org.bukkit.event.EventHandler
 import org.bukkit.plugin.Plugin
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 class ComponentViewModule(plugin: Plugin) : HandleListener(plugin) {
@@ -13,9 +15,7 @@ class ComponentViewModule(plugin: Plugin) : HandleListener(plugin) {
     @Suppress("unused")
     @EventHandler
     fun onRegisterComponentService(event: ComponentServiceRegisterEvent) {
-        event.addModule(module(createdAtStart = true) {
-            singleOf(::ViewPlayerLoader)
-        })
+        event.addModule(module { single { ViewPlayerLoader(get(named("view-player")), get()) } })
         event.registerResource("view-resource")
         event.registerInstance("view-player")
 
