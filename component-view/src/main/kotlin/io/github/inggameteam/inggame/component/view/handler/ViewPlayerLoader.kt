5570c826d0a7159f038c00e1ffc68bdbb765ce9a
Bruce0203 <BruceFreedy@gmail.com>
2023-01-29 02:48:18 +0900

    test: WIP

--- component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/handler/ViewPlayerLoader.kt
+++ component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/handler/ViewPlayerLoader.kt
@@ -1,9 +1,10 @@
 package io.github.inggameteam.inggame.component.view.handler
 
+import io.github.inggameteam.inggame.component.componentservice.ComponentService
 import io.github.inggameteam.inggame.component.componentservice.LayeredComponentService
 import io.github.inggameteam.inggame.component.helper.LayeredPlayerLoader
 import io.github.inggameteam.inggame.utils.IngGamePlugin
 
-class ViewPlayerLoader(layeredComponentService: LayeredComponentService, plugin: IngGamePlugin) :
-    LayeredPlayerLoader(layeredComponentService, plugin) {
+class ViewPlayerLoader(componentService: ComponentService, plugin: IngGamePlugin) :
+    LayeredPlayerLoader(componentService as LayeredComponentService, plugin) {
 }
