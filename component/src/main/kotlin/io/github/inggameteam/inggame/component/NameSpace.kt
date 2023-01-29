afe109a890ce15fa7cf17491c5f17063722a5f51
Bruce0203 <BruceFreedy@gmail.com>
2023-01-30 00:09:29 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/NameSpace.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/NameSpace.kt
@@ -42,6 +42,7 @@ fun encodeNameSpace(ns: NameSpace, codec: MongoCodec): Document {
     return Document().apply {
         set("_id", ns.name)
         set("parents", ns.parents.map { it.toString() }.toList())
+        println(get("parents"))
         set("elements", ns.elements.entries.map { entry ->
             codec.encode(entry.value).let { Document().apply { set("key", entry.key); set("value", it) } }
         }.toList())
