8bf247337c588b13a1e2acade93d34abb77ec197
Bruce0203 <BruceFreedy@gmail.com>
2023-01-30 17:31:00 +0900

    test: WIP

--- component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/ModelFieldSelector.kt
+++ component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/selector/ModelFieldSelector.kt
@@ -41,7 +41,7 @@ class ModelFieldSelector(
 
 
     override fun select(t: Field, event: InventoryClickEvent) {
-        val type = model.arguments[0].type!!
+        val type = try { t.returnType.apply { singleClass } } catch (_: Throwable) { model }
         val mView = ModelViewImp(this, type)
         app.get<EditorRegistry>().getEditor(
             type, this, this,
