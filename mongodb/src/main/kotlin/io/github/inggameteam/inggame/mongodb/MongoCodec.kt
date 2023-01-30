3fff9f271f7a5e5fea0b115d77cb38c18111f952
Bruce0203 <BruceFreedy@gmail.com>
2023-01-30 21:22:09 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
@@ -19,7 +19,7 @@ class MongoCodec(codecs: Collection<Class<*>>) {
     fun decode(document: Any?): Any? {
         if (document === null) return null
         if (document is Document) {
-            return codecRegistry[Class.forName(document.getString("_t"))]
+            return codecRegistry[Class.forName(document.getString("_t").apply { println(this) })]
                 .decode(
                     document.toBsonDocument().asBsonReader(),
                     DecoderContext.builder().checkedDiscriminator(true).build()
