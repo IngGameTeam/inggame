5c50c18b1b1b67dd6958dea16f6f9d812ca1696e
Bruce0203 <BruceFreedy@gmail.com>
2023-01-30 16:12:20 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/wrapper/game/GameImp.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/wrapper/game/GameImp.kt
@@ -11,6 +11,7 @@ import org.bukkit.Bukkit
 import java.util.*
 import java.util.concurrent.CopyOnWriteArraySet
 
+@PropWrapper
 class GameImp(wrapper: Wrapper) : Game, Wrapper by wrapper {
 
     override val uuid: UUID get() = nameSpace as UUID
