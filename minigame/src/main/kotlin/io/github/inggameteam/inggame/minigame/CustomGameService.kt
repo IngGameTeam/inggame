4fe24b45dc049aae0b2995f7a0824663e3c48b10
Bruce0203 <BruceFreedy@gmail.com>
2023-02-02 00:49:17 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/CustomGameService.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/CustomGameService.kt
@@ -4,3 +4,8 @@ import io.github.inggameteam.inggame.component.componentservice.ComponentService
 import io.github.inggameteam.inggame.component.componentservice.LayeredComponentService
 
 class CustomGameService(componentService: ComponentService) : ComponentService by componentService
+{
+    init {
+        println("CustomGameService=${componentService.javaClass.simpleName}")
+    }
+}
