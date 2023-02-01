5e35d780c83b5d965aeea7434677df131ce7dc0e
Bruce0203 <BruceFreedy@gmail.com>
2023-02-01 12:55:45 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/wrapper/player/GPlayer.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/wrapper/player/GPlayer.kt
@@ -2,13 +2,14 @@ package io.github.inggameteam.inggame.minigame.wrapper.player
 
 import io.github.inggameteam.inggame.component.delegate.Wrapper
 import io.github.inggameteam.inggame.minigame.wrapper.game.Game
+import io.github.inggameteam.inggame.minigame.wrapper.game.GameImp
 import io.github.inggameteam.inggame.player.warpper.WrappedPlayer
 import io.github.inggameteam.inggame.utils.TagContainer
 import java.util.concurrent.CopyOnWriteArraySet
 
 class GPlayer(wrapper: Wrapper) : WrappedPlayer(wrapper), TagContainer {
 
-    var joinedGame: Game? by nullable
+    var joinedGame: GameImp? by nullable
     override var tags: CopyOnWriteArraySet<String> by default { CopyOnWriteArraySet<String>() }
 
 }
