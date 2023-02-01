e32f749be0ed188e0fcfe7c575226074e2d8f136
Bruce0203 <BruceFreedy@gmail.com>
2023-02-01 22:30:15 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/Module.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/Module.kt
@@ -46,5 +46,5 @@ fun createRepo(name: String, collection: String) = module {
 
 
 fun createFileRepo(name: String, fileDir: String) = module {
-    factory(named(name)) { MongoFileRepo(fileDir, get()) } bind MongoRepo::class
+    factory(named(name)) { MongoFileRepo(fileDir, get(), get()) } bind MongoRepo::class
 }
