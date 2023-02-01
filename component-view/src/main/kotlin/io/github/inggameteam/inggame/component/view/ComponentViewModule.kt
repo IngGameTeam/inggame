6ea63a685f88150966e2a815b6e976c513bea37f
Bruce0203 <BruceFreedy@gmail.com>
2023-02-01 15:17:51 +0900

    test: WIP

--- component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/ComponentViewModule.kt
+++ component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/ComponentViewModule.kt
@@ -2,6 +2,7 @@ package io.github.inggameteam.inggame.component.view
 
 import io.github.inggameteam.inggame.component.event.ComponentServiceRegisterEvent
 import io.github.inggameteam.inggame.component.view.handler.ViewPlayerLoader
+import io.github.inggameteam.inggame.component.view.wrapper.Selector
 import io.github.inggameteam.inggame.utils.HandleListener
 import org.bukkit.event.EventHandler
 import org.bukkit.plugin.Plugin
@@ -13,6 +14,7 @@ class ComponentViewModule(plugin: Plugin) : HandleListener(plugin) {
     @Suppress("unused")
     @EventHandler
     fun onRegisterComponentService(event: ComponentServiceRegisterEvent) {
+        event.registerClass(Selector::class)
         event.addModule(module(createdAtStart = true) { single { ViewPlayerLoader(get(named("view-player")), get()) } })
         event.register {
             "view-player" isLayer true cs "view-resource" isSavable true cs "default"
