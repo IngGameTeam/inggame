e32ead909d5cc3f05fb01ffc43aa72611cae0236
Bruce0203 <BruceFreedy@gmail.com>
2023-02-02 00:38:08 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/componentservice/LayeredComponentService.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/componentservice/LayeredComponentService.kt
@@ -16,6 +16,7 @@ interface LayeredComponentService : ComponentService, SaveComponentService {
         val ns = getAll().firstOrNull { it.name == nameSpace } ?: run { throw NameSpaceNotFoundException(nameSpace) }
         return ns.elements.getOrDefault(key, null)?.run { this as T }
             ?: run {
+                println(parentComponent.name)
                 ns.parents.forEach { try { return parentComponent[it, key, clazz] } catch (_: Throwable) { } }
                 throw NameSpaceNotFoundException(nameSpace)
             }
