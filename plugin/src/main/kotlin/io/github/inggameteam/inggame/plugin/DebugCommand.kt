db95f338e31b8b369e9214f06d34146b94fbe86f
Bruce0203 <BruceFreedy@gmail.com>
2023-02-02 22:56:52 +0900

    test: WIP

--- plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/DebugCommand.kt
+++ plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/DebugCommand.kt
@@ -24,7 +24,7 @@ fun debugCommand(plugin: IngGamePlugin, app: Koin) = plugin.run {
                     val nameSpace = split[1].run { try { fastUUID() } catch (_: Throwable) { this } }
                     val key = split[2]
                     measureTimeMillis {
-                        repeat (1000) {
+                        repeat (20000) {
                             source.sendMessage(componentService[nameSpace, key, Any::class].toString())
                         }
                     }.run(Any::toString).apply(source::sendMessage)
