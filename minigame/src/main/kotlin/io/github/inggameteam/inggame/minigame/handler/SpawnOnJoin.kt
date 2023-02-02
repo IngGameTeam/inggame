c33295993341a5c36804f89d2456cc446ae5cbb9
Bruce0203 <BruceFreedy@gmail.com>
2023-02-03 02:08:29 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/SpawnOnJoin.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/handler/SpawnOnJoin.kt
@@ -2,6 +2,7 @@ package io.github.inggameteam.inggame.minigame.handler
 
 import io.github.inggameteam.inggame.component.Handler
 import io.github.inggameteam.inggame.component.PropWrapper
+import io.github.inggameteam.inggame.minigame.event.GPlayerSpawnEvent
 import io.github.inggameteam.inggame.minigame.event.GameJoinEvent
 import io.github.inggameteam.inggame.utils.HandleListener
 import io.github.inggameteam.inggame.utils.IngGamePlugin
@@ -9,11 +10,13 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 
 @PropWrapper
-class SpawnOnJoin(plugin: IngGamePlugin) : Handler, HandleListener(plugin) {
+class SpawnOnJoin(val plugin: IngGamePlugin) : Handler, HandleListener(plugin) {
 
     @Suppress("unused")
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onJoinGame(event: GameJoinEvent) {
+        if (isNotHandler(event.game)) return
+        plugin.server.pluginManager.callEvent(GPlayerSpawnEvent(event.player))
     }
 
 }
