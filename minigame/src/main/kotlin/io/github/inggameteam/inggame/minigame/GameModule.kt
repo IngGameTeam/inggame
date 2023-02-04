317fb9cd8cf038076a52c9935fcbcea27de8516c
Bruce0203 <BruceFreedy@gmail.com>
2023-02-04 13:38:26 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/GameModule.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/GameModule.kt
@@ -17,8 +17,6 @@ class GameModule(plugin: Plugin) : HandleListener(plugin) {
     @Suppress("unused")
     @EventHandler
     fun onRegisterComponentService(event: ComponentServiceRegisterEvent) {
-        println(Reflections("io.github.inggameteam.inggame.minigame.base").getSubTypesOf(Wrapper::class.java))
-
         event.addModule(module(createdAtStart = true) {
             singleOf(::JoinHubOnJoinServer)
             singleOf(::PrintOnMove)
