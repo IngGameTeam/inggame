e2fe2799a20c33f5bb9d57b61656d36d1253645e
Bruce0203 <BruceFreedy@gmail.com>
2023-02-04 18:46:15 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/game/GameServer.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/game/GameServer.kt
@@ -5,6 +5,6 @@ import io.github.inggameteam.inggame.component.delegate.Wrapper
 class GameServer(wrapper: Wrapper) : Wrapper by wrapper {
 
     var hub: GameImp by nonNull
-    var gameWorld: String by default { "customized_world_game" }
+    var gameWorld: String by default { "customized_game_world" }
 
 }
