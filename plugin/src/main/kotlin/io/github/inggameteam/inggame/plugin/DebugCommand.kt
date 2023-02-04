891ec05534092d60f8a4240a419fa45b825290a8
Bruce0203 <BruceFreedy@gmail.com>
2023-02-04 13:53:28 +0900

    test: WIP

--- plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/DebugCommand.kt
+++ plugin/src/main/kotlin/io/github/inggameteam/inggame/plugin/DebugCommand.kt
@@ -27,8 +27,8 @@ fun debugCommand(plugin: IngGamePlugin, app: Koin) = plugin.run {
                     val key = split[2]
                     measureTimeMillis {
                         repeat (2000) {
-                            componentService.get(nameSpace, ::GPlayer)
-//                            source.sendMessage(componentService[nameSpace, key, Any::class].toString())
+//                            componentService.get(nameSpace, ::GPlayer)
+                            source.sendMessage(componentService[nameSpace, key, Any::class].toString())
                         }
                     }.run(Any::toString).apply(source::sendMessage)
                 }
