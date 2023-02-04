f54933826389a11d68e4000af1375e1dce192f9b
Bruce0203 <BruceFreedy@gmail.com>
2023-02-04 15:59:35 +0900

    test: WIP

--- minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/PrintOnMove.kt
+++ minigame/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/PrintOnMove.kt
@@ -8,6 +8,7 @@ import io.github.inggameteam.inggame.utils.IngGamePlugin
 import io.github.inggameteam.inggame.utils.randomUUID
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerMoveEvent
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.system.measureTimeMillis
 
 class PrintOnMove(
@@ -19,9 +20,30 @@ class PrintOnMove(
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
         val player = gamePlayerService[event.player.uniqueId, ::GPlayer]
+        val map = mapOf(
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+            randomUUID() to randomUUID(),
+        ).run(::ConcurrentHashMap)
         measureTimeMillis{
+            val key = randomUUID()
             repeat (100){
-                repeat(10000) { run { } }
+                repeat(10000) { map.getOrDefault(key, null) }
 //                if (isHandler(player)) {
 //                    player[::GameAlertImp].GAME_JOIN.send(player)
 //                    event.player.sendMessage("${randomUUID()}")
