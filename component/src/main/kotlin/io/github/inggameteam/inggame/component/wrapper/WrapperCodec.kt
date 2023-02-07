e3544f9f94cd159b2e0550043995940fdd981d67
Bruce0203 <BruceFreedy@gmail.com>
2023-02-07 17:29:54 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/wrapper/WrapperCodec.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/wrapper/WrapperCodec.kt
@@ -6,11 +6,14 @@ import org.bson.BsonWriter
 import org.bson.codecs.Codec
 import org.bson.codecs.DecoderContext
 import org.bson.codecs.EncoderContext
+import org.koin.ext.getFullName
 
 @NoArgsConstructor
 class WrapperCodec : Codec<WrapperModel> {
     override fun encode(writer: BsonWriter?, value: WrapperModel?, encoderContext: EncoderContext?) {
         if (value != null && writer != null) {
+            writer.writeStartDocument()
+            writer.writeString("_t", value::class.getFullName())
             writer.writeString(WrapperModel::componentService.name, value.componentService)
             writer.writeString(WrapperModel::nameSpace.name, value.nameSpace)
             writer.writeString(WrapperModel::wrapperClass.name, value.wrapperClass)
