1940c48de18d476d9a712040790f1321c14f844d
Bruce0203 <BruceFreedy@gmail.com>
2023-02-08 01:32:27 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/event/ComponentServiceRegisterEvent.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/event/ComponentServiceRegisterEvent.kt
@@ -56,7 +56,7 @@ class ComponentServiceRegisterEvent(
 
     fun getNewModule() = getRegistry().let { registry ->
         registry.map { cs ->
-            println(cs)
+//            println(cs)
             module {
                 includes(createFileRepo(cs.name, cs.name))
                 single(named(cs.name)) {
