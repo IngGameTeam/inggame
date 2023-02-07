2a7070a9819e1179f95ffb33572d079ba775c314
Bruce0203 <BruceFreedy@gmail.com>
2023-02-07 17:05:11 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentModule.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentModule.kt
@@ -19,7 +19,7 @@ class ComponentModule(val plugin: IngGamePlugin) : Listener(plugin) {
     @EventHandler
     fun onRegisterComponentService(event: ComponentServiceRegisterEvent) {
         event.addModule(module(createdAtStart = true) {
-            factory {DecodeFunction { if (it is WrapperModel) it.createWrapper(getKoin()) else null } }
+//            factory {DecodeFunction { if (it is WrapperModel) it.createWrapper(getKoin()) else null } }
             factory {EncodeFunction { if (it is Wrapper) WrapperModel(it) else null } }
         })
         event.register { "singleton" cs "default" }
