efe7482803e6929fe64bdf68c235e075c9d44ecc
Bruce0203 <BruceFreedy@gmail.com>
2023-02-07 17:17:35 +0900

    test: WIP

--- mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
+++ mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
@@ -64,7 +64,7 @@ class MongoCodec(
         } else {
             var v: Any = value
             encodeFunctionAll.list.forEach { it.code.invoke(v)?.apply { v = this } }
-            return v
+            return encode(v)
         }
     }
 
