87c309bf26931265f83e19df83f64f6464707080
Bruce0203 <BruceFreedy@gmail.com>
2023-02-08 01:22:38 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentModule.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentModule.kt
@@ -1,17 +1,21 @@
 package io.github.inggameteam.inggame.component
 
+import io.github.inggameteam.inggame.component.componentservice.ComponentService
 import io.github.inggameteam.inggame.component.event.ComponentServiceRegisterEvent
 import io.github.inggameteam.inggame.component.model.*
 import io.github.inggameteam.inggame.component.wrapper.Wrapper
 import io.github.inggameteam.inggame.component.wrapper.WrapperCodec
 import io.github.inggameteam.inggame.component.wrapper.WrapperModel
+import io.github.inggameteam.inggame.mongodb.ClassRegistryAll
 import io.github.inggameteam.inggame.mongodb.DecodeFunction
 import io.github.inggameteam.inggame.mongodb.EncodeFunction
 import io.github.inggameteam.inggame.utils.IngGamePlugin
 import io.github.inggameteam.inggame.utils.Listener
 import org.bukkit.event.EventHandler
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import kotlin.reflect.full.isSubclassOf
 
 class ComponentModule(val plugin: IngGamePlugin) : Listener(plugin) {
 
@@ -19,11 +23,27 @@ class ComponentModule(val plugin: IngGamePlugin) : Listener(plugin) {
     @EventHandler
     fun onRegisterComponentService(event: ComponentServiceRegisterEvent) {
         event.addModule(module(createdAtStart = true) {
-            factory {DecodeFunction { if (it is WrapperModel) it.createWrapper(getKoin()) else null } }
-            factory {EncodeFunction { if (it is Wrapper) WrapperModel(it) else null } }
+            factory { DecodeFunction { if (it is WrapperModel) it.createWrapper(getKoin()) else null } }
+            factory { EncodeFunction { if (it is Wrapper) WrapperModel(it) else null } }
+            single {
+                val handler = get<ComponentService>(named("handler"))
+                val classes = get<ClassRegistryAll>()
+                classes.classes
+                    .filter { it.isSubclassOf(Handler::class) }
+                    .forEach {
+                        val name = it.simpleName!!
+                        if (!handler.has(name, name)) {
+                            handler.addNameSpace(name)
+                            handler.set(name, name, true)
+                        }
+                    }
+            }
         })
         event.register {
-            cs("singleton", isSavable = true) cs "default"
+            this
+                .cs("singleton", isSavable = true)
+                .cs("handler", isSavable = true)
+                .cs("default")
         }
         event.registerClass(
             WrapperModel::class,
--- minigame/src/test/kotlin/io/github/inggameteam/inggame/minigame/test/StringEqualsPerformanceTest.kt
+++ minigame/src/test/kotlin/io/github/inggameteam/inggame/minigame/test/StringEqualsPerformanceTest.kt
@@ -1,12 +1,16 @@
 package io.github.inggameteam.inggame.minigame.test
 
+import io.github.inggameteam.inggame.utils.fastForEach
 import java.util.*
 import kotlin.system.measureTimeMillis
 
+fun randomUUIDString() = String(UUID.randomUUID().toString().toByteArray())
+
 fun main() {
-    val str1 = String(UUID.randomUUID().toString().toByteArray())
-    val str2 = String(UUID.randomUUID().toString().toByteArray())
+    val list = arrayListOf<String>()
+    repeat(100000) { list.add(randomUUIDString()) }
+    val key = randomUUIDString()
     measureTimeMillis {
-        repeat(100000) { str1.equals(str2) }
+
     }.apply { println(this) }
 }
