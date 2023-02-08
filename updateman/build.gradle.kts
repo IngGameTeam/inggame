4f0db409d6bf564e1a69cd3478e5bd9ee4b41461
Bruce0203 <BruceFreedy@gmail.com>
2023-02-08 17:06:49 +0900

    test: WIP

--- updateman/build.gradle.kts
+++ updateman/build.gradle.kts
@@ -1,9 +1,7 @@
 dependencies {
     api("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
     compileOnly("commons-io:commons-io:2.6")
+    api(plugman)
+    api(command)
 }
 
-projectDependencies(
-    plugman,
-    command
-)
