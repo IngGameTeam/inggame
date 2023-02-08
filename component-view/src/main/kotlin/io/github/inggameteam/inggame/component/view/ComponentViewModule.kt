1c5eb6792c2e59780a6b6fd17d1520b9f5e55027
Bruce0203 <BruceFreedy@gmail.com>
2023-02-08 20:12:27 +0900

    test: WIP

--- component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/ComponentViewModule.kt
+++ component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/ComponentViewModule.kt
@@ -1,5 +1,7 @@
 package io.github.inggameteam.inggame.component.view
 
+import io.github.bruce0203.gui.GuiFrame
+import io.github.bruce0203.gui.GuiWindow
 import io.github.inggameteam.inggame.component.event.ComponentServiceRegisterEvent
 import io.github.inggameteam.inggame.component.view.handler.ViewPlayerLoader
 import io.github.inggameteam.inggame.component.view.wrapper.Selector
@@ -9,7 +11,7 @@ import org.bukkit.event.EventHandler
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-class ComponentViewModule(plugin: IngGamePlugin) : Listener(plugin) {
+class ComponentViewModule(val plugin: IngGamePlugin) : Listener(plugin) {
 
     @Suppress("unused")
     @EventHandler
@@ -18,6 +20,10 @@ class ComponentViewModule(plugin: IngGamePlugin) : Listener(plugin) {
         event.addModule(module(createdAtStart = true) { single { ViewPlayerLoader(get(named("view-player")), get()) } })
         event.register {
             "view-player" isMask true cs "view-resource" isSavable true cs "default"
+        }
+        plugin.addDisableEvent {
+            plugin.server.onlinePlayers.filter { it.openInventory.topInventory.holder is GuiWindow }
+                .forEach { it.closeInventory() }
         }
     }
 }
