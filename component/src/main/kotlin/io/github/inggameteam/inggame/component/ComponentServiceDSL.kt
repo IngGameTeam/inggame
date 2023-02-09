798deff4daa77e2afffb1d460b60d5f45d36d812
Bruce0203 <BruceFreedy@gmail.com>
2023-02-09 20:20:26 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentServiceDSL.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentServiceDSL.kt
@@ -109,7 +109,6 @@ fun ComponentServiceDSL.createComponentModule(): Module = this.let { cs ->
     module {
         includes(createFileRepo(cs.name, cs.name))
         single(named(cs.name)) {
-            println(cs.name)
             (if (cs.parents.isEmpty()) EmptyComponentServiceImp(cs.name)
             else if (cs.type == MULTI || cs.key !== null && cs.type !== LAYER) {
                 val root by lazy { get<ComponentService>(named(cs.root ?: "root is not exists")) }
