ad872fdb571e1b364a87bd5055ab438e59211b62
Bruce0203 <BruceFreedy@gmail.com>
2023-02-09 20:15:45 +0900

    test: WIP

--- component/src/main/kotlin/io/github/inggameteam/inggame/component/loader/ComponentLoader.kt
+++ component/src/main/kotlin/io/github/inggameteam/inggame/component/loader/ComponentLoader.kt
@@ -29,10 +29,11 @@ fun loadComponents() = module(createdAtStart = true) {
         val event = ComponentLoadEvent(eventDsl)
         get<IngGamePlugin>().server.pluginManager.callEvent(event)
         val csModules = eventDsl.registry.map(ComponentServiceDSL::createComponentModule)
+        getKoin().createEagerInstances()
+
         getKoin().loadModules(csModules)
         val modules = event.modules
         getKoin().loadModules(modules)
-        getKoin().createEagerInstances()
         ComponentLoader()
     } bind ComponentLoader::class
 }
