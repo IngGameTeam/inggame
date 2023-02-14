f9887767dd8b962ef65ab57a162c644aabe478f6
Bruce0203 <BruceFreedy@gmail.com>
2023-02-15 03:05:06 +0900

    fix: get new plugin instance by changing to function from variable

--- plugin/updateman/src/main/kotlin/io/github/inggameteam/inggame/updateman/UpdateWatchDog.kt
+++ plugin/updateman/src/main/kotlin/io/github/inggameteam/inggame/updateman/UpdateWatchDog.kt
@@ -1,31 +1,30 @@
 package io.github.inggameteam.inggame.updateman
 
-import io.github.inggameteam.inggame.utils.IngGamePlugin
 import io.github.inggameteam.inggame.utils.runNow
 import kotlin.concurrent.thread
 
 class UpdateWatchDog(
     private val settings: UpdateSettings,
     private val updateHelper: UpdateHelper,
-    private val plugin: IngGamePlugin
 ) {
+
 
     init {
         var function: () -> Unit = {}
         function = {
             thread {
                 update()
-                function.runNow(plugin)
+                function.runNow(settings.plugin)
             }
         }
-        function.runNow(plugin)
+        function.runNow(settings.plugin)
     }
 
     private fun update(): Unit = updateHelper.run {
         if (updateGit(settings)) {
-            plugin.logger.info("Updating...")
+            settings.plugin.logger.info("Updating...")
             runBuild(settings)
-            plugin.logger.info("Update Done!")
+            settings.plugin.logger.info("Update Done!")
             deploy(settings)
         }
     }
