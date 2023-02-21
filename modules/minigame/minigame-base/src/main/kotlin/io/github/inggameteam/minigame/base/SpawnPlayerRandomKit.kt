315be5118e596575adc1ac5616bd2345dcc64f3f
Bruce0203 <BruceFreedy@gmail.com>
2023-02-22 01:23:30 +0900

    feat(SpawnPlayerRandomKit): "random" kit implemented

--- modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/SpawnPlayerRandomKit.kt
+++ modules/minigame/minigame-base/src/main/kotlin/io/github/inggameteam/minigame/base/SpawnPlayerRandomKit.kt
@@ -3,6 +3,9 @@ package io.github.inggameteam.minigame.base
 import io.github.inggameteam.alert.Lang.lang
 import io.github.inggameteam.minigame.GameState
 import io.github.inggameteam.player.GPlayer
+import org.bukkit.Bukkit
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.Inventory
 
 interface SpawnPlayerRandomKit : SpawnPlayer {
 
@@ -13,5 +16,22 @@ interface SpawnPlayerRandomKit : SpawnPlayer {
         }
         super.spawn(player, comp.stringListOrNull("spawn", player.lang(plugin))?.random()?: spawn)
     }
+
+    override fun inventorySpawn(player: GPlayer, spawn: String): Inventory? {
+        if (spawn == "random") {
+            run {
+                val inventory = Bukkit.createInventory(null, InventoryType.PLAYER)
+                val lang = player.lang(plugin)
+                comp.stringListOrNull("random-kit-slots", lang)?.forEach {
+                    val i = it.toInt()
+                    val randomItem = comp.stringListOrNull("random-kit-$it", lang)?.random() ?: return@run
+                    inventory.setItem(i, comp.itemOrNull(randomItem, lang))
+                }
+                return inventory
+            }
+        }
+        return super.inventorySpawn(player, spawn)
+    }
+
 
 }
