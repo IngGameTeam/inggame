dd9e7e8a539439b0f7a440d701bbb03a09d941fb
Bruce0203 <BruceFreedy@gmail.com>
2023-02-21 15:43:56 +0900

    chore: trivial

--- server.sh
+++ server.sh
@@ -15,15 +15,15 @@
 #  wget -O server.jar https://ci.codemc.io/job/etil2jz/job/Mirai-1.19/lastSuccessfulBuild/artifact/build/libs/mirai-paperclip-1.19.2-R0.1-SNAPSHOT-reobf.jar
 
 # Purpur
-#  VERSION=$(<version.txt)
-#  wget -O server.jar https://api.purpurmc.org/v2/purpur/$VERSION/latest/download
+  VERSION=$(<version.txt)
+  wget -O server.jar https://api.purpurmc.org/v2/purpur/$VERSION/latest/download
 
 # pufferfish
 # wget -O server.jar https://ci.pufferfish.host/job/Pufferfish-1.19/lastBuild/artifact/build/libs/pufferfish-paperclip-1.19.3-R0.1-SNAPSHOT-reobf.jar
 #java -Xms12G -Xmx12G -jar server.jar
 
 # Fusion
-wget -O server.jar https://github.com/IPECTER/Fusion/releases/latest/download/Fusion-paperclip-1.19.3-R0.1-SNAPSHOT-reobf.jar
+#wget -O server.jar https://github.com/IPECTER/Fusion/releases/latest/download/Fusion-paperclip-1.19.3-R0.1-SNAPSHOT-reobf.jar
 cd customized_game_world
 rm -fvr !"paper-world.yml"
 cd ..
