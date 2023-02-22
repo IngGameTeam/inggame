050f953204521f58d8cd9a8268407953b774e2ce
Bruce0203 <BruceFreedy@gmail.com>
2023-02-22 18:29:24 +0900

    chore: trivial

---
+++ botapp/bot.js
@@ -0,0 +1,18 @@
+const mineflayer = require('mineflayer')
+
+
+var createBot = function() {
+    let bot = mineflayer.createBot({
+        host: 'localhost',
+        username: 'test_bot',
+        port: 25555,
+    })
+    bot.on('chat', (username, message) => {
+      if (username === bot.username) return
+      bot.chat(message)
+    })
+    bot.on('kicked', console.log)
+    bot.on('error', console.log)
+}
+
+
