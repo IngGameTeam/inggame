b8322f594ba5f065856e3977e3777b212847990b
Bruce0203 <BruceFreedy@gmail.com>
2023-02-27 01:27:17 +0900

    chore: trivial

--- server.sh
+++ server.sh
@@ -26,4 +26,4 @@
 rm -r customized_game_world
 git stash
 git stash drop
-java -Xms12G -Xmx16G -XX:MaxGCPauseMillis=50 -XX:ParallelGCThreads=6 -XX:ConcGCThreads=3 -jar --add-modules jdk.incubator.vector server.jar
+java -Xms10G -Xmx10G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar --add-modules jdk.incubator.vector server.jar
