2574273bb7390b3eeb19558b7a139449fd160ac3
Bruce0203 <BruceFreedy@gmail.com>
2023-03-22 23:33:15 +0900

    chore: trivial

--- modules/standard/player/src/main/kotlin/io/github/inggameteam/inggame/player/container/ContainerHelperBase.kt
+++ modules/standard/player/src/main/kotlin/io/github/inggameteam/inggame/player/container/ContainerHelperBase.kt
@@ -48,7 +48,10 @@ abstract class ContainerHelperBase<CONTAINER : Container<ELEMENT>, ELEMENT : Con
             containerHelper.join(container, element)
             println(measureTimeMillis{
                 container.joinedPlayers.forEach { p ->
-                    p[::ContainerAlertImp].GAME_JOIN.send(p, element, container.containerName)
+                    p[::ContainerAlertImp].GAME_JOIN
+                    p
+                    element
+                    container.containerName
                 }
             })
             if (joinType === JoinType.PLAY) element.isPlaying = true
