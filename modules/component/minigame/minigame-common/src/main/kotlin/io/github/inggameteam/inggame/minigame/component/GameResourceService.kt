91d4385f5e687920bdc53dc16520433914622333
Bruce0203 <BruceFreedy@gmail.com>
2023-03-27 00:32:14 +0900

    chore: trivial

--- modules/component/minigame/minigame-common/src/main/kotlin/io/github/inggameteam/inggame/minigame/component/GameResourceService.kt
+++ modules/component/minigame/minigame-common/src/main/kotlin/io/github/inggameteam/inggame/minigame/component/GameResourceService.kt
@@ -1,8 +1,8 @@
 package io.github.inggameteam.inggame.minigame.component
 
-import io.github.inggameteam.inggame.component.Resource
+import io.github.inggameteam.inggame.component.Multi
 import io.github.inggameteam.inggame.component.componentservice.ComponentService
 
-@Resource("game")
+@Multi("game")
 class GameResourceService(componentService: ComponentService)
     : ComponentService by componentService
