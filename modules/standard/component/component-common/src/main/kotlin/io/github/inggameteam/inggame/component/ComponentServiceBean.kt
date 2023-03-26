dd75f72b33c39aa663c46a9711a8c36b405014bd
Bruce0203 <BruceFreedy@gmail.com>
2023-03-27 01:07:12 +0900

    chore: trivial

--- modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentServiceBean.kt
+++ modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentServiceBean.kt
@@ -90,6 +90,7 @@ class ComponentServiceBean(val plugin: IngGamePlugin) : Listener(plugin) {
                                                         ?.also {
                                                             registry.first().parents.removeIf { cs -> cs == parentName }
                                                             this@appendLinked.parents.remove(parentName)
+                                                            this@appendLinked.parents.remove("handler")
                                                             this@appendLinked.cs(parentName).apply { parents.addAll(it.parents) }
                                                         }
                                                         ?: this@appendLinked.cs(parentName, type = LINKED).apply { cs("handler") }
