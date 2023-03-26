dae58d055d38141d1932140c3b4bc02cfdb51e95
Bruce0203 <BruceFreedy@gmail.com>
2023-03-27 00:23:46 +0900

    chore: trivial

--- modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentServiceBean.kt
+++ modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/ComponentServiceBean.kt
@@ -93,7 +93,7 @@ class ComponentServiceBean(val plugin: IngGamePlugin) : Listener(plugin) {
                                                             println("-".repeat(10))
                                                             return if (registeredParent !== null && registeredParent.type !== type) {
                                                                 val isRemoved = registry.removeIf { it.name == parentName }
-                                                                println("$name=$isRemoved")
+                                                                println("$parentName=$isRemoved")
                                                                 cs(parentName, type = type)
                                                             } else if (registeredParent === null) {
                                                                 cs(parentName, type = LINKED)
