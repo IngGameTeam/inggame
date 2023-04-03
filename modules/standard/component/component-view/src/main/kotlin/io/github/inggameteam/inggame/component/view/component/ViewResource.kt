77665b06ffcb52e3c2d72ddc1e9f62c2e01b24ca
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 19:28:15 +0900

    chore: trivial

---
+++ modules/standard/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/component/ViewResource.kt
@@ -0,0 +1,8 @@
+package io.github.inggameteam.inggame.component.view.component
+
+import io.github.inggameteam.inggame.component.Resource
+import io.github.inggameteam.inggame.component.componentservice.ComponentService
+
+@Resource("view")
+class ViewResource(componentService: ComponentService) {
+}
