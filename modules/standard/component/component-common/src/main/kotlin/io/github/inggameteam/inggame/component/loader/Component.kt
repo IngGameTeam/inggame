27a01abf5ecef9a4847720e34ec08853670dcf7f
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 19:35:47 +0900

    chore: trivial

--- modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/loader/Component.kt
+++ modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/loader/Component.kt
@@ -1,11 +1,12 @@
 package io.github.inggameteam.inggame.component.loader
 
 import io.github.inggameteam.inggame.component.wrapper.Wrapper
+import io.github.inggameteam.inggame.utils.SafeSetWithToString
 
 interface Component : Wrapper {
 
     val componentType: ComponentServiceType
     val isSavable: Boolean
-    val parentsList: ArrayList<String>
+    val parentsList: SafeSetWithToString<String>
 
 }
--- modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/loader/ComponentImp.kt
+++ modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/loader/ComponentImp.kt
@@ -2,9 +2,10 @@ package io.github.inggameteam.inggame.component.loader
 
 import io.github.inggameteam.inggame.component.wrapper.SimpleWrapper
 import io.github.inggameteam.inggame.component.wrapper.Wrapper
+import io.github.inggameteam.inggame.utils.SafeSetWithToString
 
 class ComponentImp(wrapper: Wrapper) : Component, SimpleWrapper(wrapper) {
     override val componentType: ComponentServiceType by nonNull
     override val isSavable: Boolean by nonNull
-    override val parentsList: ArrayList<String> by nonNull
+    override val parentsList: SafeSetWithToString<String> by nonNull
 }
