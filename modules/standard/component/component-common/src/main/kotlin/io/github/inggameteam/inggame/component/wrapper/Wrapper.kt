4a2eca06742e00d629c479ea611ebb5c67c7a45d
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 22:09:16 +0900

    chore: trivial

--- modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/wrapper/Wrapper.kt
+++ modules/standard/component/component-common/src/main/kotlin/io/github/inggameteam/inggame/component/wrapper/Wrapper.kt
@@ -123,7 +123,6 @@ class NonNullWrapperImp(
                 if (defaultValue === null) throw e
                 defaultValue
             }
-            println("prop=${property.name}, type=${result.javaClass.simpleName}")
             return result as R
         } catch (e: Throwable) {
             throw AssertionError("'$nameSpace' name space '${property.name}' key '${thisRef.javaClass.simpleName}' ref not exist")
