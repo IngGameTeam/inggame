8fd3cb87c9193f05496e5fb42960ac05aa05ca66
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 19:49:17 +0900

    chore: trivial

--- modules/standard/mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
+++ modules/standard/mongodb/src/main/kotlin/io/github/inggameteam/inggame/mongodb/MongoCodec.kt
@@ -2,10 +2,7 @@ package io.github.inggameteam.inggame.mongodb
 
 import com.mongodb.MongoClientSettings
 import io.github.inggameteam.inggame.utils.Model
-import org.bson.BsonArray
-import org.bson.BsonDocument
-import org.bson.BsonDocumentWriter
-import org.bson.Document
+import org.bson.*
 import org.bson.codecs.Codec
 import org.bson.codecs.DecoderContext
 import org.bson.codecs.EncoderContext
@@ -56,7 +53,15 @@ class MongoCodec(
 
     fun encode(value: Any?): Any? {
         if (value === null) return null
-        return if (value.javaClass.getAnnotation(Model::class.java) !== null) {
+        return if (value is String) return BsonString(value)
+        else if (value is Int) return BsonInt32(value)
+        else if (value is Long) return BsonInt64(value)
+        else if (value is Double || value is Float) return BsonDouble(value as Double)
+        else if (value is Char) return BsonString(value.toString())
+        else if (value is Boolean) return BsonBoolean(value)
+        else if (value is Short) return BsonInt32(value.toInt())
+        else if (value is Byte) return BsonInt32(value.toInt())
+        else if (value.javaClass.getAnnotation(Model::class.java) !== null) {
             val document = BsonDocument()
             val writer = BsonDocumentWriter(document)
             codecRegistry[value.javaClass].encode(writer, value, EncoderContext.builder().build())
