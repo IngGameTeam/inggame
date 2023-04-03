1193f6a3687c8af6af3b236f43148414d7599b85
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 18:45:04 +0900

    chore: trivial

--- modules/standard/item/src/main/kotlin/io/github/inggameteam/inggame/item/component/ItemResource.kt
+++ modules/standard/item/src/main/kotlin/io/github/inggameteam/inggame/item/component/ItemResource.kt
@@ -1,6 +1,8 @@
 package io.github.inggameteam.inggame.item.component
 
+import io.github.inggameteam.inggame.component.Resource
 import io.github.inggameteam.inggame.component.componentservice.ComponentService
 
+@Resource("item")
 class ItemResource(componentService: ComponentService)
     : ComponentService by componentService
