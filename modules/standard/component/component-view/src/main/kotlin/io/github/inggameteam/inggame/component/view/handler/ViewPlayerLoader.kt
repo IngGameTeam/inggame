0a37996c30fe377ebc9d9249688c2fc2959462d1
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 19:13:47 +0900

    chore: trivial

--- modules/standard/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/handler/ViewPlayerLoader.kt
+++ modules/standard/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/handler/ViewPlayerLoader.kt
@@ -3,7 +3,9 @@ package io.github.inggameteam.inggame.component.view.handler
 import io.github.inggameteam.inggame.component.componentservice.LayeredComponentService
 import io.github.inggameteam.inggame.component.helper.LayeredPlayerLoader
 import io.github.inggameteam.inggame.component.view.component.ViewPlayer
+import io.github.inggameteam.inggame.utils.Helper
 import io.github.inggameteam.inggame.utils.IngGamePlugin
 
-class ViewPlayerLoader(componentService: ViewPlayer, plugin: IngGamePlugin) :
-    LayeredPlayerLoader(componentService as LayeredComponentService, plugin)
+@Helper
+class ViewPlayerLoader(viewHelper: ViewPlayer, plugin: IngGamePlugin) :
+    LayeredPlayerLoader(viewHelper as LayeredComponentService, plugin)
