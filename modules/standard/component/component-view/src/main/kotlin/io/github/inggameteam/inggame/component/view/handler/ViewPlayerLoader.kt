24df64ac54a7a9cfa23e325f38bc3ca3e68b2e73
Bruce0203 <BruceFreedy@gmail.com>
2023-04-03 19:14:31 +0900

    chore: trivial

--- modules/standard/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/handler/ViewPlayerLoader.kt
+++ modules/standard/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/handler/ViewPlayerLoader.kt
@@ -5,7 +5,13 @@ import io.github.inggameteam.inggame.component.helper.LayeredPlayerLoader
 import io.github.inggameteam.inggame.component.view.component.ViewPlayer
 import io.github.inggameteam.inggame.utils.Helper
 import io.github.inggameteam.inggame.utils.IngGamePlugin
+import org.bukkit.Bukkit
 
 @Helper
 class ViewPlayerLoader(viewHelper: ViewPlayer, plugin: IngGamePlugin) :
     LayeredPlayerLoader(viewHelper as LayeredComponentService, plugin)
+{
+        init {
+            Bukkit.broadcastMessage("ViewPlayerLoader")
+        }
+    }
