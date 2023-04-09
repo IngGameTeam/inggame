b70dcecffd3d2a3951b1f1576a2789417ff86fb6
Bruce0203 <BruceFreedy@gmail.com>
2023-04-09 14:59:05 +0900

    chore: trivial

---
+++ modules/component/minigame/minigame-common/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/spawnplayer/TeleportOnStop.kt
@@ -0,0 +1,37 @@
+package io.github.inggameteam.inggame.minigame.base.spawnplayer
+
+import io.github.inggameteam.inggame.component.HandleListener
+import io.github.inggameteam.inggame.component.model.LocationModel
+import io.github.inggameteam.inggame.component.wrapper.SimpleWrapper
+import io.github.inggameteam.inggame.component.wrapper.Wrapper
+import io.github.inggameteam.inggame.minigame.base.game.GameImp
+import io.github.inggameteam.inggame.minigame.base.game.event.GPlayerSpawnEvent
+import io.github.inggameteam.inggame.minigame.base.sectional.SectionalImp
+import io.github.inggameteam.inggame.utils.ContainerState
+import io.github.inggameteam.inggame.utils.IngGamePlugin
+import org.bukkit.event.EventHandler
+
+interface TeleportOnStop : Wrapper {
+    val teleportOnStop: LocationModel?
+}
+
+class TeleportOnStopImp(wrapper: Wrapper) : SimpleWrapper(wrapper), TeleportOnStop{
+    override val teleportOnStop: LocationModel? by nullable
+}
+
+@Deprecated("not referenced")
+class TeleportOnStopHandler(plugin: IngGamePlugin) : HandleListener(plugin) {
+    @Suppress("unused")
+    @EventHandler
+    fun onGamePlayerSpawn(event: GPlayerSpawnEvent) {
+        val player = event.player
+        val game = player.joined[::SectionalImp]
+        if (isNotHandler(game)) return
+        if (game.containerState !== ContainerState.STOP) return
+        val location = game[::TeleportOnSpawnImp].teleportOnSpawn
+            ?.run { game.toRelative(this) }
+            ?: return
+        player.teleport(location)
+    }
+
+}
