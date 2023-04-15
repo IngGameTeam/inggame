a95d45807c4e7bb313b0d02835b26970346e0627
Bruce0203 <BruceFreedy@gmail.com>
2023-04-15 17:27:05 +0900

    chore: trivial

--- modules/common/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/controller/ElementSelector.kt
+++ modules/common/component/component-view/src/main/kotlin/io/github/inggameteam/inggame/component/view/controller/ElementSelector.kt
@@ -59,8 +59,10 @@ class ElementSelector(nameSpaceView: NameSpaceView, override val parentSelector:
     override fun select(t: Pair<Any, Any>, event: InventoryClickEvent) {
         val prop = app.get<PropertyRegistry>().getProp(t.first.toString())
         val type = if (prop.type.singleClass.isInterface)
-            try { componentService.find(nameSpace.name, t.first.toString(), Any::class)::class.createType() }
-            catch(e: Exception) { e.printStackTrace(); prop.type }
+            try { componentService.find(nameSpace.name, t.first.toString(), Any::class)::class
+                .apply { println(this.simpleName) }
+                .createType() }
+            catch(e: Exception) { prop.type }
         else prop.type
         app.get<EditorRegistry>().getEditor(type, ElementViewImp(this, t), this)
             .open(event.whoClicked as Player)
