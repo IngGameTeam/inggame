9c805f2220631209b892c81ca9d5341e422cdb33
Bruce0203 <BruceFreedy@gmail.com>
2023-04-15 17:36:55 +0900

    chore: trivial

--- modules/component/minigame/minigame-common/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/spawnplayer/TeleportOnSpawn.kt
+++ modules/component/minigame/minigame-common/src/main/kotlin/io/github/inggameteam/inggame/minigame/base/spawnplayer/TeleportOnSpawn.kt
@@ -8,6 +8,7 @@ import io.github.inggameteam.inggame.minigame.base.game.event.GPlayerSpawnEvent
 import io.github.inggameteam.inggame.minigame.base.sectional.SectionalImp
 import io.github.inggameteam.inggame.player.ContainerState
 import io.github.inggameteam.inggame.utils.IngGamePlugin
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 
 interface TeleportOnSpawn : Wrapper {
@@ -26,11 +27,15 @@ class TeleportOnSpawnHandler(plugin: IngGamePlugin) : HandleListener(plugin) {
     fun onGamePlayerSpawn(event: GPlayerSpawnEvent) {
         val player = event.player
         val game = player.joined[::SectionalImp]
+        Bukkit.broadcastMessage("TeleportOnSpawn.1")
         if (isNotHandler(game)) return
+        Bukkit.broadcastMessage("TeleportOnSpawn.2")
         if (game.containerState !== ContainerState.PLAY) return
+        Bukkit.broadcastMessage("TeleportOnSpawn.3")
         val location = game[::TeleportOnSpawnImp].teleportOnSpawn
             ?.run { game.toRelative(this) }
             ?: return
+        Bukkit.broadcastMessage("TeleportOnSpawn.4")
         player.teleport(location)
     }
 
