33f5a44c20ec9e3f6a05fd9824710c0493d31ec4
Bruce0203 <BruceFreedy@gmail.com>
2023-04-23 20:44:08 +0900

    fix: add Fusion bukkit

--- server.sh
+++ server.sh
@@ -22,7 +22,7 @@
 #java -Xms12G -Xmx12G -jar server.jar
 
 # Fusion
-#wget -O server.jar https://github.com/IPECTER/Fusion/releases/latest/download/Fusion-paperclip-1.19.3-R0.1-SNAPSHOT-reobf.jar
+wget -O server.jar https://github.com/IPECTER/Fusion/releases/latest/download/Fusion-paperclip-1.19.3-R0.1-SNAPSHOT-reobf.jar
 rm -r customized_game_world
 git stash
 git stash drop
